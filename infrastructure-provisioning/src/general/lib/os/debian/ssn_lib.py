345157ab72669031d9cb05aba44731ac340dc247
Vadym_Kuznetsov <Vadym_Kuznetsov@epam.com>
2017-05-18 17:04:53 +0300

    [EPMCBDCCSS-1237]: typo fix

--- infrastructure-provisioning/src/general/lib/os/debian/ssn_lib.py
+++ infrastructure-provisioning/src/general/lib/os/debian/ssn_lib.py
@@ -207,7 +207,7 @@ def start_ss(keyfile, host_string, dlab_conf_dir, web_path, os_user, mongo_passw
                 sudo('keytool -exportcert -alias dlab -storepass PASSWORD -file /home/{0}/keys/dlab.crt \
                      -keystore /home/{0}/keys/dlab.keystore.jks'.format(os_user))
                 sudo('keytool -importcert -trustcacerts -alias dlab -file /home/{}/keys/dlab.crt -noprompt \
-                     -storepass changeit -keystore /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts')
+                     -storepass changeit -keystore /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts'.format(os_user))
             except:
                 append_result("Unable to generate cert and copy to java keystore")
                 sys.exit(1)
