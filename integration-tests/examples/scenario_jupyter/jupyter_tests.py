e84bdc00da6cd9b4db2f18531aef541402b0d61c
Vadym Kuznetsov <Vadym_Kuznetsov@epam.com>
2017-06-21 23:05:43 +0300

    [EPMCDLAB-764]: debug boto3 s3 client

--- integration-tests/examples/scenario_jupyter/jupyter_tests.py
+++ integration-tests/examples/scenario_jupyter/jupyter_tests.py
@@ -62,8 +62,8 @@ def run_ipynb(ipynb_name):
 def prepare_templates():
     templates_dir = '/home/{}/'.format(args.os_user)
     local('mkdir -p {}'.format(templates_dir))
-    s3client = boto3.client('s3', region_name=args.region)
-    s3resource = boto3.resource('s3')
+    s3client = boto3.client('s3', config=Config(signature_version='s3v4'), endpoint_url='https://s3-{}.amazonaws.com'.format(args.region), region_name=args.region)
+    s3resource = boto3.resource('s3', config=Config(signature_version='s3v4'))
     get_files(s3client, s3resource, 'test_templates_jupyter', args.bucket, templates_dir)
     local('mv /home/{0}/test_templates_jupyter /home/{0}/test_templates'.format(args.os_user))
 
