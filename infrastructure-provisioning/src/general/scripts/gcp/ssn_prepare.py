fd40eae7a90c5d62378ecdbc1d753fb266ffaefd
Oleh Martushevskyi <oleh_martushevskyi@epam.com>
2017-07-03 16:17:30 +0300

    [EPMCDLAB-780]: changed name of firewall

--- infrastructure-provisioning/src/general/scripts/gcp/ssn_prepare.py
+++ infrastructure-provisioning/src/general/scripts/gcp/ssn_prepare.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     GCPActions().create_subnet(subnet_name, vpc_cidr, network_selfLink, region)
 
     params = {}
-    params['name'] = 'ssh22'
+    params['name'] = service_base_name + '-ssn-firewall'
     params['sourceRanges'] = ['0.0.0.0/0']
     rule = {}
     rule['IPProtocol'] = 'tcp'
