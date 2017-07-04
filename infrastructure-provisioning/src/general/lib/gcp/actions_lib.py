70e9f26d0c6aff87853abf6cc49d18679b86de37
Oleh Martushevskyi <oleh_martushevskyi@epam.com>
2017-07-04 18:36:13 +0300

    [EPMCDLAB-780]: adding GCP for SSN

--- infrastructure-provisioning/src/general/lib/gcp/actions_lib.py
+++ infrastructure-provisioning/src/general/lib/gcp/actions_lib.py
@@ -22,7 +22,7 @@ from oauth2client.client import GoogleCredentials
 from oauth2client.service_account import ServiceAccountCredentials
 from google.cloud import storage
 from googleapiclient import errors
-from meta_lib import GCPMeta
+from .meta_lib import GCPMeta
 import os
 import logging
 import traceback
