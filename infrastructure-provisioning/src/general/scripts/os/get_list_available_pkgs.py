f98c4144fb503cc724af5a4cfbaee141fbcbcaf6
Vadym Kuznetsov <vadym_kuznetsov@epam.com>
2017-07-18 12:13:37 +0300

    [EPMCDLAB-823]: typofix

--- infrastructure-provisioning/src/general/scripts/os/get_list_available_pkgs.py
+++ infrastructure-provisioning/src/general/scripts/os/get_list_available_pkgs.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
     all_pkgs['os_pkg'] = get_available_os_pkgs()
     all_pkgs['pip2'] = get_available_pip_pkgs("2.7")
     all_pkgs['pip3'] = get_available_pip_pkgs("3.5")
-    all_pkgs['other'] = get_uncategorised_pip_pkgs(all_pkgs['pip2'], all_pkgs['pip3'])
+    all_pkgs['others'] = get_uncategorised_pip_pkgs(all_pkgs['pip2'], all_pkgs['pip3'])
 
     if os.environ['application'] in ['jupyter', 'rstudio', 'zeppelin', 'deeplearning']:
         all_pkgs['r_pkg'] = get_available_r_pkgs()
