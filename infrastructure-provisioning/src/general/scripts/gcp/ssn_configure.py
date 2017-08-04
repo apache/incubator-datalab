953679dcea7bd5d9ab83e8378d5831cc56c01209
Oleh Moskovych <Oleh_Moskovych@epam.com>
2017-08-04 11:31:06 +0300

    Disable emr build for gcp cloud

--- infrastructure-provisioning/src/general/scripts/gcp/ssn_configure.py
+++ infrastructure-provisioning/src/general/scripts/gcp/ssn_configure.py
@@ -184,7 +184,6 @@ if __name__ == "__main__":
                              {"name": "zeppelin", "tag": "latest"},
                              {"name": "tensor", "tag": "latest"},
                              {"name": "deeplearning", "tag": "latest"},
-                             {"name": "emr", "tag": "latest"},
                              {"name": "dataproc", "tag": "latest"}]
         params = "--hostname {} --keyfile {} --additional_config '{}' --os_family {} --os_user {} --dlab_path {} --cloud_provider {} --region {}". \
             format(instance_hostname, ssn_conf['ssh_key_path'], json.dumps(additional_config),
