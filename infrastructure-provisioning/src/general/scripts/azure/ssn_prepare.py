e8163e1fe6aeb1f6a01d4a6f2154a0893fb15efe
Vadym Kuznetsov <vadym_kuznetsov@epam.com>
2017-08-15 17:51:07 +0300

    [EPMCDLAB-884][Azure]: fixed creation security group for ssn

--- infrastructure-provisioning/src/general/scripts/azure/ssn_prepare.py
+++ infrastructure-provisioning/src/general/scripts/azure/ssn_prepare.py
@@ -188,7 +188,7 @@ if __name__ == "__main__":
                     "direction": "Outbound"
                 }
             ]
-            params = "--resource_group_name {} --security_group_name {} --region {} --list_rules {}".\
+            params = "--resource_group_name {} --security_group_name {} --region {} --list_rules '{}'".\
                 format(ssn_conf['service_base_name'], ssn_conf['security_groups_name'], ssn_conf['region'], list_rules)
             try:
                 local("~/scripts/{}.py {}".format('common_create_security_group', params))
