8246335b6c6d9c331c82fb9b54fd0c9281ecbe68
Oleh Martushevskyi <oleh_martushevskyi@epam.com>
2017-08-30 16:57:53 +0300

    [EPMCDLAB-889][Azure]: fixed issues

--- infrastructure-provisioning/src/general/scripts/azure/dataengine_terminate.py
+++ infrastructure-provisioning/src/general/scripts/azure/dataengine_terminate.py
@@ -28,8 +28,8 @@ import os
 import uuid
 
 
-def terminate_data_engine(resource_group_name, master_instance_name, slave_instance_name, master_security_group_name,
-                          slave_security_group_name, notebook_name, os_user, key_path, cluster_name):
+def terminate_data_engine(resource_group_name, master_instance_name, slave_instance_name, notebook_name, os_user,
+                          key_path, cluster_name):
     print "Terminating Master node"
     try:
         AzureActions().remove_instance(resource_group_name, master_instance_name)
@@ -41,18 +41,6 @@ def terminate_data_engine(resource_group_name, master_instance_name, slave_insta
             if slave_instance_name in vm.name:
                 AzureActions().remove_instance(resource_group_name, vm.name)
                 print "Instance {} has been terminated".format(vm.name)
-    except:
-        sys.exit(1)
-    print "Removing Master security groups"
-    try:
-        AzureActions().remove_security_group(resource_group_name, master_security_group_name)
-        print "Security group {} has been terminated".format(master_security_group_name)
-    except:
-        sys.exit(1)
-    print "Removing Slave security groups"
-    try:
-        AzureActions().remove_security_group(resource_group_name, slave_security_group_name)
-        print "Security group {} has been terminated".format(slave_security_group_name)
     except:
         sys.exit(1)
 
@@ -89,10 +77,6 @@ if __name__ == "__main__":
     data_engine['slave_node_name'] = data_engine['service_base_name'] + '-' + os.environ['edge_user_name'] + \
                                       '-dataengine-' + data_engine['exploratory_name'] + '-' + \
                                       data_engine['computational_name'] + '-slave'
-    data_engine['master_security_group_name'] = data_engine['service_base_name'] + '-' \
-                                                + os.environ['edge_user_name'] + '-dataengine-master-sg'
-    data_engine['slave_security_group_name'] = data_engine['service_base_name'] + '-' \
-                                               + os.environ['edge_user_name'] + '-dataengine-slave-sg'
     data_engine['notebook_name'] = os.environ['notebook_instance_name']
     data_engine['key_path'] = os.environ['conf_key_dir'] + '/' + os.environ['conf_key_name'] + '.pem'
     data_engine['cluster_name'] = \
@@ -104,8 +88,7 @@ if __name__ == "__main__":
         print '[TERMINATE DATA ENGINE]'
         try:
             terminate_data_engine(data_engine['resource_group_name'], data_engine['master_node_name'],
-                                  data_engine['slave_node_name'], data_engine['master_security_group_name'],
-                                  data_engine['slave_security_group_name'], data_engine['notebook_name'],
+                                  data_engine['slave_node_name'], data_engine['notebook_name'],
                                   os.environ['conf_os_user'], data_engine['key_path'], data_engine['cluster_name'])
         except Exception as err:
             traceback.print_exc()
