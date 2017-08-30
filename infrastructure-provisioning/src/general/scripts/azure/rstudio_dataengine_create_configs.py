88ad6a7793eb2069137eb888796874d616b842b5
Vadym Kuznetsov <vadym_kuznetsov@epam.com>
2017-08-30 11:43:47 +0300

    [EPMCDLAB-884][Azure]: fixed configuring rstudio for dataengine

--- infrastructure-provisioning/src/general/scripts/azure/rstudio_dataengine_create_configs.py
+++ infrastructure-provisioning/src/general/scripts/azure/rstudio_dataengine_create_configs.py
@@ -39,11 +39,9 @@ parser.add_argument('--hadoop_version', type=str, default='')
 parser.add_argument('--region', type=str, default='')
 parser.add_argument('--os_user', type=str, default='')
 parser.add_argument('--spark_master', type=str, default='')
-parser.add_argument('--keyfile', type=str, default='')
-parser.add_argument('--notebook_ip', type=str, default='')
 args = parser.parse_args()
 
-spark_dir = '/opt/' + args.emr_version + '/' + args.cluster_name + '/spark/'
+spark_dir = '/opt/' + args.cluster_name + '/spark/'
 spark_version = args.spark_version
 hadoop_version = args.hadoop_version
 spark_link = "http://d3kbcqa49mib13.cloudfront.net/spark-" + spark_version + "-bin-hadoop" + hadoop_version + ".tgz"
