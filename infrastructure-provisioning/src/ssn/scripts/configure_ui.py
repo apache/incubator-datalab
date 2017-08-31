ed82cee47de09eca650fc43abd533435f5bd3459
Oleh Martushevskyi <oleh_martushevskyi@epam.com>
2017-08-31 19:42:27 +0300

    [EPMCDLAB-889][Azure]: fixed issue

--- infrastructure-provisioning/src/ssn/scripts/configure_ui.py
+++ infrastructure-provisioning/src/ssn/scripts/configure_ui.py
@@ -63,18 +63,14 @@ keystore_passwd = id_generator()
 
 
 def copy_ssn_libraries():
-    try:
-        sudo('mkdir -p /usr/lib/python2.7/dlab/')
-        run('mkdir -p /tmp/dlab_libs/')
-        local('scp -i {} /usr/lib/python2.7/dlab/* {}:/tmp/dlab_libs/'.format(args.keyfile, env.host_string))
-        run('chmod a+x /tmp/dlab_libs/*')
-        sudo('mv /tmp/dlab_libs/* /usr/lib/python2.7/dlab/')
-        if exists('/usr/lib64'):
-            sudo('ln -fs /usr/lib/python2.7/dlab /usr/lib64/python2.7/dlab')
-    except Exception as err:
-        print "Errrorrr ---->>"
-        print err
-
+    sudo('mkdir -p /usr/lib/python2.7/dlab/')
+    run('mkdir -p /tmp/dlab_libs/')
+    local('scp -i {} /usr/lib/python2.7/dlab/* {}:/tmp/dlab_libs/'.format(args.keyfile, env.host_string))
+    run('chmod a+x /tmp/dlab_libs/*')
+    sudo('mv /tmp/dlab_libs/* /usr/lib/python2.7/dlab/')
+    if exists('/usr/lib64'):
+        sudo('ln -fs /usr/lib/python2.7/dlab /usr/lib64/python2.7/dlab')
+    return True
 
 def configure_mongo(mongo_passwd):
     try:
@@ -118,14 +114,11 @@ if __name__ == "__main__":
         deeper_config = json.loads(args.additional_config)
     except:
         sys.exit(2)
-    try:
-        print "Copying DLab libraries to SSN"
-        if not copy_ssn_libraries():
-            logging.error('Failed to copy DLab libraries')
-            sys.exit(1)
-    except Exception as err:
-        print "Errrorrr ---->>"
-        print err
+
+    print "Copying DLab libraries to SSN"
+    if not copy_ssn_libraries():
+        logging.error('Failed to copy DLab libraries')
+        sys.exit(1)
 
     print "Installing Supervisor"
     if not ensure_supervisor():
