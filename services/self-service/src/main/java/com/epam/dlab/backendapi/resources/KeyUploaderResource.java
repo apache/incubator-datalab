4cb7db18ea6b227c2e1d17541dc4923127079aba
YuriyHolinkoEp <Yuriy_Holinko@epam.com>
2017-09-05 09:02:10 -0400

    [EPMCDLAB-962] Edge key upload return value

--- services/self-service/src/main/java/com/epam/dlab/backendapi/resources/KeyUploaderResource.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/resources/KeyUploaderResource.java
@@ -189,6 +189,8 @@ public class KeyUploaderResource implements EdgeAPI {
                 UploadFileAws uploadFileAws = new UploadFileAws();
                 uploadFileAws.setEdge(edge);
                 uploadFileAws.setContent(content);
+
+                return uploadFileAws;
             }
             case AZURE: {
                 EdgeCreateAzure edge = ResourceUtils.newResourceSysBaseDTO(userInfo, EdgeCreateAzure.class, cloudProvider)
@@ -201,6 +203,8 @@ public class KeyUploaderResource implements EdgeAPI {
                 UploadFileAzure uploadFileAzure = new UploadFileAzure();
                 uploadFileAzure.setEdge(edge);
                 uploadFileAzure.setContent(content);
+
+                return uploadFileAzure;
             }
             case GCP:
             default:
