8af14e4c153ec958e98941362b7edce5e7318aa1
YuriyHolinkoEp <Yuriy_Holinko@epam.com>
2017-09-22 07:08:05 -0400

    Allow to decrese login timeout

--- services/security-service/src/main/java/com/epam/dlab/auth/SecurityServiceConfiguration.java
+++ services/security-service/src/main/java/com/epam/dlab/auth/SecurityServiceConfiguration.java
@@ -1,117 +1,103 @@
-/***************************************************************************
+/*
+ * Copyright (c) 2017, EPAM SYSTEMS INC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
- Copyright (c) 2016, EPAM SYSTEMS INC
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-****************************************************************************/
 
 package com.epam.dlab.auth;
 
+import com.epam.dlab.ServiceConfiguration;
+import com.epam.dlab.auth.dao.Request;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.directory.ldap.client.api.LdapConnectionConfig;
+
+import javax.validation.constraints.Min;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.directory.ldap.client.api.LdapConnectionConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.epam.dlab.auth.dao.Request;
-import com.epam.dlab.ServiceConfiguration;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import javax.validation.constraints.Min;
-
+@Slf4j
 public class SecurityServiceConfiguration extends ServiceConfiguration {
-
-	protected final Logger LOG = LoggerFactory.getLogger(this.getClass());
-
-	public SecurityServiceConfiguration() {
-		super();
-	}
-
-	private boolean userInfoPersistenceEnabled = false;
-
-	@JsonProperty
-	private boolean awsUserIdentificationEnabled = false;
-
-	@JsonProperty
-	@Min(10)
+    @JsonProperty
+    private boolean userInfoPersistenceEnabled = false;
+    @JsonProperty
+    private boolean awsUserIdentificationEnabled = false;
+    @JsonProperty
+    @Min(5)
     private int loginAuthenticationTimeout = 10;
+    @JsonProperty
+    private long inactiveUserTimeoutMillSec;
+    @JsonProperty
+    private List<Request> ldapSearch;
+    @JsonProperty
+    private String ldapBindTemplate;
+    @JsonProperty
+    private String ldapBindAttribute;
+    @JsonProperty
+    private String ldapSearchAttribute;
+    @JsonProperty
+    private Map<String, String> ldapConnectionConfig = new HashMap<>();
+    private LdapConnectionConfig _ldapConnectionConfig;
 
-	@JsonProperty
-	private long inactiveUserTimeoutMillSec;
+    public SecurityServiceConfiguration() {
+        super();
+    }
 
-	public long getInactiveUserTimeoutMillSec() {
-		return inactiveUserTimeoutMillSec;
-	}
+    public long getInactiveUserTimeoutMillSec() {
+        return inactiveUserTimeoutMillSec;
+    }
 
-	@JsonProperty
-	public boolean isUserInfoPersistenceEnabled() {
-		return userInfoPersistenceEnabled;
-	}
+    public boolean isUserInfoPersistenceEnabled() {
+        return userInfoPersistenceEnabled;
+    }
 
-	@JsonProperty
-	private List<Request> ldapSearch;
+    public List<Request> getLdapSearch() {
+        return ldapSearch;
+    }
 
-	public List<Request> getLdapSearch() {
-		return ldapSearch;
-	}
+    public LdapConnectionConfig getLdapConnectionConfig() {
+        if (_ldapConnectionConfig == null) {
+            _ldapConnectionConfig = new LdapConnectionConfig();
+            _ldapConnectionConfig.setLdapHost(ldapConnectionConfig.get("ldapHost"));
+            _ldapConnectionConfig.setLdapPort(Integer.parseInt(ldapConnectionConfig.get("ldapPort")));
+            _ldapConnectionConfig.setName(ldapConnectionConfig.get("name"));
+            _ldapConnectionConfig.setCredentials(ldapConnectionConfig.get("credentials"));
+            //TODO: add all configurable properties
+            //      from the LdapConnectionConfig class
+        }
+        return _ldapConnectionConfig;
 
-	@JsonProperty
-	private String ldapBindTemplate;
+    }
 
-	@JsonProperty
-	private String ldapBindAttribute;
+    public String getLdapBindTemplate() {
+        return ldapBindTemplate;
+    }
 
-	@JsonProperty
-	private String ldapSearchAttribute;
+    public String getLdapBindAttribute() {
+        return ldapBindAttribute;
+    }
 
-	@JsonProperty
-	private Map<String,String> ldapConnectionConfig = new HashMap<String, String>();
-	private LdapConnectionConfig _ldapConnectionConfig;
+    public String getLdapSearchAttribute() {
+        return ldapSearchAttribute;
+    }
 
-	public LdapConnectionConfig getLdapConnectionConfig() {
-		if(_ldapConnectionConfig == null) {
-			_ldapConnectionConfig = new LdapConnectionConfig();
-			_ldapConnectionConfig.setLdapHost(ldapConnectionConfig.get("ldapHost"));
-			_ldapConnectionConfig.setLdapPort(Integer.parseInt(ldapConnectionConfig.get("ldapPort")));
-			_ldapConnectionConfig.setName(ldapConnectionConfig.get("name"));
-			_ldapConnectionConfig.setCredentials(ldapConnectionConfig.get("credentials"));
-			//TODO: add all configurable properties
-			//      from the LdapConnectionConfig class
-		}
-		return _ldapConnectionConfig;
+    public boolean isAwsUserIdentificationEnabled() {
+        return awsUserIdentificationEnabled;
+    }
 
-	}
-
-	public String getLdapBindTemplate() {
-		return ldapBindTemplate;
-	}
-
-	public String getLdapBindAttribute() {
-		return ldapBindAttribute;
-	}
-
-	public String getLdapSearchAttribute() {
-		return ldapSearchAttribute;
-	}
-
-	public boolean isAwsUserIdentificationEnabled() {
-		return awsUserIdentificationEnabled;
-	}
-
-	public int getLoginAuthenticationTimeout() {
-		return loginAuthenticationTimeout;
-	}
+    public int getLoginAuthenticationTimeout() {
+        return loginAuthenticationTimeout;
+    }
 }
