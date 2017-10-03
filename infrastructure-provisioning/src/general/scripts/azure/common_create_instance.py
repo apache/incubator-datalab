57ea6bb9fa009abda9200c0b73964daba36d0858
Vadym Kuznetsov <vadym_kuznetsov@epam.com>
2017-10-03 15:54:54 +0300

    [EPMCDLAB-1089]: fixed virtual machine tagging

--- infrastructure-provisioning/src/general/scripts/azure/common_create_instance.py
+++ infrastructure-provisioning/src/general/scripts/azure/common_create_instance.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
                 AzureActions().create_instance(args.region, args.instance_size, args.service_base_name,
                                                args.instance_name, args.dlab_ssh_user_name, args.public_key,
                                                network_interface_id, args.resource_group_name, args.primary_disk_size,
-                                               args.instance_type, args.ami_name, args.tags, args.user_name,
+                                               args.instance_type, args.ami_name, json.loads(args.tags), args.user_name,
                                                create_option, disk_id, args.instance_storage_account_type)
         except:
             sys.exit(1)
