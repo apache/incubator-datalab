ff8af8d6b47a86614feec649759f43929b2ff183
Vadym Kuznetsov <vadym_kuznetsov@epam.com>
2017-10-06 15:06:50 +0300

    [EPMCDLAB-1087]: fixed length of service_base_name to 12 symbols in Azure

--- infrastructure-provisioning/src/general/scripts/azure/ssn_prepare.py
+++ infrastructure-provisioning/src/general/scripts/azure/ssn_prepare.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
         pre_defined_sg = False
         ssn_conf = dict()
         # We need to cut service_base_name to 12 symbols do to the Azure Name length limitation
-        ssn_conf['service_base_name'] = os.environ['conf_service_base_name'].replace('_', '-')[:13]
+        ssn_conf['service_base_name'] = os.environ['conf_service_base_name'].replace('_', '-')[:12]
         ssn_conf['vpc_name'] = ssn_conf['service_base_name'] + '-vpc'
         ssn_conf['subnet_name'] = ssn_conf['service_base_name'] + '-ssn-subnet'
         ssn_conf['region'] = os.environ['azure_region']
