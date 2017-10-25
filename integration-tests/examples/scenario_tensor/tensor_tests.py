5457281493bb233a9c2d954605601d970bc42bea
Oleh Moskovych <Oleh_Moskovych@epam.com>
2017-10-25 18:31:34 +0300

    [EPMCDLAB-938]: Added tests for all libraries for DeepLearning template

--- integration-tests/examples/scenario_tensor/tensor_tests.py
+++ integration-tests/examples/scenario_tensor/tensor_tests.py
@@ -63,64 +63,16 @@ def prepare_templates():
     templates_dir = '/home/{}/'.format(args.os_user)
     s3client = boto3.client('s3', config=Config(signature_version='s3v4'), endpoint_url='https://s3-{}.amazonaws.com'.format(args.region), region_name=args.region)
     s3resource = boto3.resource('s3', config=Config(signature_version='s3v4'), endpoint_url='https://s3-{}.amazonaws.com'.format(args.region), region_name=args.region)
-    get_files(s3client, s3resource, 'test_templates_deeplearning', args.bucket, templates_dir)
-    local('mv /home/{0}/test_templates_deeplearning /home/{0}/test_templates'.format(args.os_user))
+    get_files(s3client, s3resource, 'test_templates_tensor', args.bucket, templates_dir)
+    local('mv /home/{0}/test_templates_tensor /home/{0}/test_templates'.format(args.os_user))
 
 
-def run_caffe():
+def run_tensor():
     interpreters = ['pyspark_local', 'pyspark_' + args.cluster_name]
     for i in interpreters:
-        prepare_ipynb(i, '/home/{}/test_templates/template_caffe.ipynb'.format(args.os_user), 'test_caffe')
-        run_ipynb('test_caffe')
-
-
-def run_caffe2():
-    interpreters = ['pyspark_local', 'pyspark_' + args.cluster_name]
-    for i in interpreters:
-        prepare_ipynb(i, '/home/{}/test_templates/template_caffe2.ipynb'.format(args.os_user), 'test_caffe2')
-        run_ipynb('test_caffe2')
-
-
-def run_cntk():
-    interpreters = ['pyspark_local', 'pyspark_' + args.cluster_name]
-    for i in interpreters:
-        prepare_ipynb(i, '/home/{}/test_templates/template_cntk.ipynb'.format(args.os_user), 'test_cntk')
-        run_ipynb('test_cntk')
-
-
-def run_keras():
-    interpreters = ['pyspark_local', 'pyspark_' + args.cluster_name]
-    for i in interpreters:
-        prepare_ipynb(i, '/home/{}/test_templates/template_keras.ipynb'.format(args.os_user), 'test_keras')
-        run_ipynb('test_keras')
-
-
-def run_mxnet():
-    interpreters = ['pyspark_local', 'pyspark_' + args.cluster_name]
-    for i in interpreters:
-        prepare_ipynb(i, '/home/{}/test_templates/template_mxnet.ipynb'.format(args.os_user), 'test_mxnet')
-        run_ipynb('test_mxnet')
-
-
-def run_theano():
-    interpreters = ['pyspark_local', 'pyspark_' + args.cluster_name]
-    for i in interpreters:
-        prepare_ipynb(i, '/home/{}/test_templates/template_theano.ipynb'.format(args.os_user), 'test_theano')
-        run_ipynb('test_theano')
-
-
-def run_torch():
-    interpreters = ['itorch']
-    for i in interpreters:
-        prepare_ipynb(i, '/home/{}/test_templates/template_torch.ipynb'.format(args.os_user), 'test_torch')
-        run_ipynb('test_torch')
+        prepare_ipynb(i, '/home/{}/test_templates/template_tensor.ipynb'.format(args.os_user), 'tensor')
+        run_ipynb('tensor')
 
 
 prepare_templates()
-run_caffe()
-run_caffe2()
-run_cntk()
-run_keras()
-run_mxnet()
-run_theano()
-run_torch()
+run_tensor()
