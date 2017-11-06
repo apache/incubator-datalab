dd8041870729727030c07a464976fbcba9f56786
Oleh Martushevskyi <oleh_martushevskyi@epam.com>
2017-11-06 18:05:19 +0200

    [EPMCDLAB-1092]: fixed issue with ssn_hostname

--- infrastructure-provisioning/src/general/scripts/azure/common_prepare_notebook.py
+++ infrastructure-provisioning/src/general/scripts/azure/common_prepare_notebook.py
@@ -51,8 +51,8 @@ if __name__ == "__main__":
         if edge_status != 'running':
             logging.info('ERROR: Edge node is unavailable! Aborting...')
             print('ERROR: Edge node is unavailable! Aborting...')
-            ssn_hostname = AzureMeta().get_instance_public_ip_address(os.environ['conf_service_base_name'],
-                                                                      os.environ['conf_service_base_name'] + '-ssn')
+            ssn_hostname = AzureMeta().get_private_ip_address(os.environ['conf_service_base_name'],
+                                                              os.environ['conf_service_base_name'] + '-ssn')
             put_resource_status('edge', 'Unavailable', os.environ['ssn_dlab_path'], os.environ['conf_os_user'],
                                 ssn_hostname)
             append_result("Edge node is unavailable")
