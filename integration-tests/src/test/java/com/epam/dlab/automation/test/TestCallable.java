012f9d93d3f8bd6dceeff5f3b0137352d810f233
YuriyHolinkoEp <Yuriy_Holinko@epam.com>
2017-11-10 16:28:12 +0200

    Updated cluster names creation for spark test

--- integration-tests/src/test/java/com/epam/dlab/automation/test/TestCallable.java
+++ integration-tests/src/test/java/com/epam/dlab/automation/test/TestCallable.java
@@ -105,7 +105,9 @@ public class TestCallable implements Callable<Boolean> {
         
         final DeployClusterDto deployClusterDto = createClusterDto();
         
-        final String actualClusterName = NamingHelper.getClusterName(NamingHelper.getClusterInstanceNameForTestEmr(notebookName, clusterName, dataEngineType));
+        final String actualClusterName = NamingHelper.getClusterName(NamingHelper.getClusterInstanceNameForTestEmr(notebookName,
+                clusterName, dataEngineType), dataEngineType);
+
         if (!ConfigPropertyValue.isRunModeLocal()) {
         	TestEmr test = new TestEmr();
         	test.run(notebookName, actualClusterName);
