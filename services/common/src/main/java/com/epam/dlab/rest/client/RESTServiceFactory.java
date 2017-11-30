bea420a5f4c0132206082583e085da13ad7ef3e1
YuriyHolinkoEp <Yuriy_Holinko@epam.com>
2017-11-30 12:36:46 +0200

    Use user agent header in REST clients

--- services/common/src/main/java/com/epam/dlab/rest/client/RESTServiceFactory.java
+++ services/common/src/main/java/com/epam/dlab/rest/client/RESTServiceFactory.java
@@ -1,20 +1,20 @@
 /***************************************************************************
 
-Copyright (c) 2016, EPAM SYSTEMS INC
+ Copyright (c) 2016, EPAM SYSTEMS INC
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 
-****************************************************************************/
+ ****************************************************************************/
 
 package com.epam.dlab.rest.client;
 
@@ -50,8 +50,12 @@ public class RESTServiceFactory {
     private JerseyClientConfiguration jerseyClientConfiguration;
 
     public RESTService build(Environment environment, String name) {
+        return build(environment, name, null);
+    }
+
+    public RESTService build(Environment environment, String name, String userAgent) {
         Client client = new JerseyClientBuilder(environment).using(jerseyClientConfiguration).build(name);
-        return new RESTService(client, getURL());
+        return new RESTService(client, getURL(), userAgent);
     }
 
     private String getURL() {
