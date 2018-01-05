e99746f31eea0911c80f8dba8e0c1c77adc75642
bohdan.hliva <bohdan.hliva@datalex.com>
2018-01-05 15:49:12 +0200

    EPMCDLAB-851 added main operation for edge node for GCP

--- services/self-service/src/main/java/com/epam/dlab/backendapi/resources/callback/gcp/EdgeCallbackGcp.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/resources/callback/gcp/EdgeCallbackGcp.java
@@ -1,4 +1,40 @@
 package com.epam.dlab.backendapi.resources.callback.gcp;
 
-public class EdgeCallbackGcp {
+import com.epam.dlab.backendapi.domain.RequestId;
+import com.epam.dlab.backendapi.resources.callback.base.EdgeCallback;
+import com.epam.dlab.dto.aws.edge.EdgeInfoAws;
+import com.epam.dlab.dto.base.keyload.UploadFileResult;
+import com.epam.dlab.rest.contracts.ApiCallbacks;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/infrastructure/edge")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Slf4j
+public class EdgeCallbackGcp extends EdgeCallback {
+
+    public EdgeCallbackGcp() {
+        log.info("{} is initialized", getClass().getSimpleName());
+    }
+
+    /**
+     * Stores the result of the upload the user key.
+     *
+     * @param dto result of the upload the user key.
+     * @return 200 OK
+     */
+    @POST
+    @Path(ApiCallbacks.STATUS_URI)
+    public Response status(UploadFileResult<EdgeInfoAws> dto) {
+        RequestId.checkAndRemove(dto.getRequestId());
+        handleEdgeCallback(dto.getUser(), dto.getStatus());
+        return Response.ok().build();
+    }
 }
