9ad923950daf15cd204c5c6a661131a9996eb6de
Oleh Moskovych <Oleh_Moskovych@epam.com>
2018-01-30 16:07:59 +0200

    [EPMCDLAB-1326]: fixed unzip

--- integration-tests/examples/scenario_tensor/tensor_tests.py
+++ integration-tests/examples/scenario_tensor/tensor_tests.py
@@ -36,7 +36,7 @@ args = parser.parse_args()
 def prepare_templates():
     try:
         local('/bin/bash -c "source /etc/profile && wget http://files.fast.ai/data/dogscats.zip -O /tmp/dogscats.zip"')
-        local('unzip -q /tmp/dogscats -d /tmp')
+        local('unzip -q /tmp/dogscats.zip -d /tmp')
         local('/bin/bash -c "mkdir -p /home/{0}/{1}"'.format(args.os_user, "{test,train}"))
         local('mv /tmp/dogscats/test1/* /home/{0}/test'.format(args.os_user))
         local('/bin/bash -c "mv /tmp/dogscats/valid/{0}/* /home/{1}/train"'.format("{cats,dogs}", args.os_user))
