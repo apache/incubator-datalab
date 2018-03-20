c14da51304248d80915e47f110800f2c67a9c1c5
bohdan.hliva <bohdan_hliva@epam.com>
2018-03-20 17:17:26 +0200

    EPMCDLAB-1437 fixed reporting labels for spark cluster + code refactor

--- services/self-service/src/main/java/com/epam/dlab/backendapi/dao/BillingDAO.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/dao/BillingDAO.java
@@ -25,9 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.bson.Document;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.epam.dlab.backendapi.dao.ComputationalDAO.COMPUTATIONAL_ID;
 import static com.epam.dlab.backendapi.dao.ExploratoryDAO.COMPUTATIONAL_RESOURCES;
@@ -37,124 +35,161 @@ import static com.mongodb.client.model.Projections.*;
 @Slf4j
 public abstract class BillingDAO extends BaseDAO {
 
-    public static final String SHAPE = "shape";
-    public static final String SERVICE_BASE_NAME = "service_base_name";
-    public static final String ITEMS = "lines";
-    public static final String COST_TOTAL = "cost_total";
-    public static final String FULL_REPORT = "full_report";
+	public static final String SHAPE = "shape";
+	public static final String SERVICE_BASE_NAME = "service_base_name";
+	public static final String ITEMS = "lines";
+	public static final String COST_TOTAL = "cost_total";
+	public static final String FULL_REPORT = "full_report";
 
-    private static final String MASTER_NODE_SHAPE = "master_node_shape";
-    private static final String SLAVE_NODE_SHAPE = "slave_node_shape";
-    private static final String TOTAL_INSTANCE_NUMBER = "total_instance_number";
+	private static final String MASTER_NODE_SHAPE = "master_node_shape";
+	private static final String SLAVE_NODE_SHAPE = "slave_node_shape";
+	private static final String TOTAL_INSTANCE_NUMBER = "total_instance_number";
 
-    private static final String DATAENGINE_SLAVE = "dataengine_slave";
-    private static final String DATAENGINE_MASTER = "dataengine_master";
-    private static final String DATAENGINE_INSTANCE_COUNT = "dataengine_instance_count";
+	private static final String DATAENGINE_SHAPE = "dataengine_instance_shape";
+	private static final String DATAENGINE_INSTANCE_COUNT = "dataengine_instance_count";
 
-    private static final String DATAENGINE_DOCKER_IMAGE = "image";
+	private static final String DATAENGINE_DOCKER_IMAGE = "image";
 
-    @Inject
-    protected SettingsDAO settings;
+	@Inject
+	protected SettingsDAO settings;
 
-    protected Map<String, BillingDAO.ShapeInfo> getShapes(List<String> shapeNames) {
-        FindIterable<Document> docs = getCollection(USER_INSTANCES)
-                .find()
-                .projection(
-                        fields(excludeId(),
-                                include(SHAPE, EXPLORATORY_ID,
-                                        COMPUTATIONAL_RESOURCES + "." + COMPUTATIONAL_ID,
-                                        COMPUTATIONAL_RESOURCES + "." + MASTER_NODE_SHAPE,
-                                        COMPUTATIONAL_RESOURCES + "." + SLAVE_NODE_SHAPE,
-                                        COMPUTATIONAL_RESOURCES + "." + TOTAL_INSTANCE_NUMBER,
-                                        COMPUTATIONAL_RESOURCES + "." + DATAENGINE_MASTER,
-                                        COMPUTATIONAL_RESOURCES + "." + DATAENGINE_SLAVE,
-                                        COMPUTATIONAL_RESOURCES + "." + DATAENGINE_INSTANCE_COUNT,
-                                        COMPUTATIONAL_RESOURCES + "." + DATAENGINE_DOCKER_IMAGE
-                                )));
-        Map<String, BillingDAO.ShapeInfo> shapes = new HashMap<>();
-        for (Document d : docs) {
-            String name = d.getString(SHAPE);
-            if (shapeNames == null || shapeNames.isEmpty() || shapeNames.contains(name)) {
-                shapes.put(d.getString(EXPLORATORY_ID), new BillingDAO.ShapeInfo(d.getString(SHAPE)));
-            }
+	protected Map<String, BillingDAO.ShapeInfo> getShapes(List<String> shapeNames) {
+		FindIterable<Document> userInstances = getUserInstances();
+		final Map<String, BillingDAO.ShapeInfo> shapes = new HashMap<>();
 
-            @SuppressWarnings("unchecked")
-            List<Document> comp = (List<Document>) d.get(COMPUTATIONAL_RESOURCES);
-            for (Document c : comp) {
-                if (DataEngineType.fromDockerImageName(c.getString(DATAENGINE_DOCKER_IMAGE)) == DataEngineType.SPARK_STANDALONE) {
-                    name = c.getString(DATAENGINE_MASTER);
-                    String slaveName = c.getString(DATAENGINE_SLAVE);
-                    if ((shapeNames == null || shapeNames.isEmpty() ||
-                            shapeNames.contains(name) || shapeNames.contains(slaveName))
-                            && StringUtils.isNotEmpty(c.getString(COMPUTATIONAL_ID))) {
+		for (Document d : userInstances) {
+			getExploratoryShape(shapeNames, d)
+					.ifPresent(shapeInfo -> shapes.put(d.getString(EXPLORATORY_ID), shapeInfo));
+			@SuppressWarnings("unchecked")
+			List<Document> comp = (List<Document>) d.get(COMPUTATIONAL_RESOURCES);
+			comp.forEach(computational ->
+					getComputationalShape(shapeNames, computational)
+							.ifPresent(shapeInfo -> shapes.put(computational.getString(COMPUTATIONAL_ID), shapeInfo)));
+		}
 
-                        shapes.put(c.getString(COMPUTATIONAL_ID),
-                                new BillingDAO.ShapeInfo(name, slaveName, c.getString(DATAENGINE_INSTANCE_COUNT)));
-                    }
-                } else {
-                    name = c.getString(MASTER_NODE_SHAPE);
-                    String slaveName = c.getString(SLAVE_NODE_SHAPE);
-                    if (shapeNames == null || shapeNames.isEmpty() ||
-                            shapeNames.contains(name) || shapeNames.contains(slaveName)) {
-                        shapes.put(c.getString(COMPUTATIONAL_ID),
-                                new BillingDAO.ShapeInfo(name, slaveName, c.getString(TOTAL_INSTANCE_NUMBER)));
-                    }
-                }
-            }
-        }
+		appendSsnAndEdgeNodeType(shapeNames, shapes);
 
-        appendSsnAndEdgeNodeType(shapeNames, shapes);
+		log.trace("Loaded shapes is {}", shapes);
+		return shapes;
+	}
 
-        log.trace("Loaded shapes is {}", shapes);
-        return shapes;
-    }
+	private Optional<ShapeInfo> getComputationalShape(List<String> shapeNames, Document c) {
+		return isDataEngine(c.getString(DATAENGINE_DOCKER_IMAGE)) ? getDataEngineShape(shapeNames, c) :
+				getDataEngineServiceShape(shapeNames, c);
+	}
 
-    protected abstract void appendSsnAndEdgeNodeType(List<String> shapeNames, Map<String, BillingDAO.ShapeInfo> shapes);
+	private FindIterable<Document> getUserInstances() {
+		return getCollection(USER_INSTANCES)
+				.find()
+				.projection(
+						fields(excludeId(),
+								include(SHAPE, EXPLORATORY_ID,
+										COMPUTATIONAL_RESOURCES + "." + COMPUTATIONAL_ID,
+										COMPUTATIONAL_RESOURCES + "." + MASTER_NODE_SHAPE,
+										COMPUTATIONAL_RESOURCES + "." + SLAVE_NODE_SHAPE,
+										COMPUTATIONAL_RESOURCES + "." + TOTAL_INSTANCE_NUMBER,
+										COMPUTATIONAL_RESOURCES + "." + DATAENGINE_SHAPE,
+										COMPUTATIONAL_RESOURCES + "." + DATAENGINE_INSTANCE_COUNT,
+										COMPUTATIONAL_RESOURCES + "." + DATAENGINE_DOCKER_IMAGE
+								)));
+	}
 
-    protected String generateShapeName(ShapeInfo shape) {
+	private Optional<ShapeInfo> getExploratoryShape(List<String> shapeNames, Document d) {
+		final String shape = d.getString(SHAPE);
+		if (isShapeAcceptable(shapeNames, shape)) {
+			return Optional.of(new ShapeInfo(shape));
+		}
+		return Optional.empty();
+	}
 
-        String shapeName = "";
+	private boolean isDataEngine(String dockerImage) {
+		return DataEngineType.fromDockerImageName(dockerImage) == DataEngineType.SPARK_STANDALONE;
+	}
 
-        if (shape != null) {
-            if (shape.isExploratory()) {
-                return shape.getShape();
-            } else {
-                try {
-                    // Total number of instances. Slave instances equals total minus one
-                    int count = Integer.parseInt(shape.getSlaveCount());
-                    return String.format("Master: %s%sSlave:  %dx%s", shape.getShape(), System.lineSeparator(),
-                            count - 1, shape.getSlaveShape());
-                } catch (NumberFormatException e) {
-                    log.error("Cannot parse string {} to integer", shape.getSlaveCount());
-                    return shapeName;
-                }
-            }
-        }
+	private Optional<ShapeInfo> getDataEngineServiceShape(List<String> shapeNames,
+														  Document c) {
+		final String desMasterShape = c.getString(MASTER_NODE_SHAPE);
+		final String desSlaveShape = c.getString(SLAVE_NODE_SHAPE);
+		if (isShapeAcceptable(shapeNames, desMasterShape, desSlaveShape)) {
+			return Optional.of(new ShapeInfo(desMasterShape, desSlaveShape, c.getString(TOTAL_INSTANCE_NUMBER)));
+		}
+		return Optional.empty();
+	}
 
-        return shapeName;
-    }
+	private Optional<ShapeInfo> getDataEngineShape(List<String> shapeNames, Document c) {
+		final String dataEngineShape = c.getString(DATAENGINE_SHAPE);
+		if ((isShapeAcceptable(shapeNames, dataEngineShape))
+				&& StringUtils.isNotEmpty(c.getString(COMPUTATIONAL_ID))) {
 
-    /**
-     * Store shape info
-     */
-    @ToString
-    @Getter
-    protected class ShapeInfo {
-        private String shape;
-        private String slaveShape;
-        private String slaveCount;
-        private boolean isExploratory;
+			return Optional.of(new ShapeInfo(dataEngineShape, c.getString(DATAENGINE_INSTANCE_COUNT)));
+		}
+		return Optional.empty();
+	}
 
-        public ShapeInfo(String shape) {
-            this.shape = shape;
-            this.isExploratory = true;
-        }
+	private boolean isShapeAcceptable(List<String> shapeNames, String... shapes) {
+		return shapeNames == null || shapeNames.isEmpty() || Arrays.stream(shapes).anyMatch(shapeNames::contains);
+	}
 
-        public ShapeInfo(String shape, String slaveShape, String slaveCount) {
-            this.shape = shape;
-            this.slaveShape = slaveShape;
-            this.slaveCount = slaveCount;
-            this.isExploratory = false;
-        }
-    }
+	protected abstract void appendSsnAndEdgeNodeType(List<String> shapeNames, Map<String, BillingDAO.ShapeInfo>
+			shapes);
+
+	protected String generateShapeName(ShapeInfo shape) {
+		return Optional.ofNullable(shape).map(ShapeInfo::getName).orElse(StringUtils.EMPTY);
+	}
+
+	/**
+	 * Store shape info
+	 */
+	@Getter
+	@ToString
+	protected class ShapeInfo {
+		private static final String DES_NAME_FORMAT = "Master: %s%sSlave:  %d x %s";
+		private static final String DE_NAME_FORMAT = "%d x %s";
+		private final boolean isDataEngine;
+		private final String shape;
+		private final String slaveShape;
+		private final String slaveCount;
+		private final boolean isExploratory;
+
+		private ShapeInfo(boolean isDataEngine, String shape, String slaveShape, String slaveCount, boolean
+				isExploratory) {
+			this.isDataEngine = isDataEngine;
+			this.shape = shape;
+			this.slaveShape = slaveShape;
+			this.slaveCount = slaveCount;
+			this.isExploratory = isExploratory;
+		}
+
+		public ShapeInfo(String shape) {
+			this(false, shape, null, null, true);
+		}
+
+		ShapeInfo(String shape, String slaveShape, String slaveCount) {
+			this(false, shape, slaveShape, slaveCount, false);
+		}
+
+
+		ShapeInfo(String shape, String slaveCount) {
+			this(true, shape, null, slaveCount, false);
+		}
+
+		public String getName() {
+			if (isExploratory) {
+				return shape;
+			} else {
+				return clusterName();
+			}
+		}
+
+		private String clusterName() {
+			try {
+				final Integer count = Integer.valueOf(slaveCount);
+				return isDataEngine ? String.format(DE_NAME_FORMAT, count, shape) :
+						String.format(DES_NAME_FORMAT, shape, System.lineSeparator(), count - 1, slaveShape);
+			} catch (NumberFormatException e) {
+				log.error("Cannot parse string {} to integer", slaveCount);
+				return StringUtils.EMPTY;
+			}
+		}
+	}
 }
