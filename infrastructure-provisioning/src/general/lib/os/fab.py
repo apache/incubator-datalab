c98e3152cd95a1250ebe26755ccd495ae702bec3
Denys_Shliakhov <denys_shliakhov@epam.com>
2018-03-23 15:09:53 +0200

    EPMCDLAB-1441 [AWS] Adedd MD to zeppelin

--- infrastructure-provisioning/src/general/lib/os/fab.py
+++ infrastructure-provisioning/src/general/lib/os/fab.py
@@ -536,13 +536,17 @@ def update_zeppelin_interpreters(multiple_clusters, r_enabled, interpreter_mode=
         else:
             data = json.loads(open(interpreters_config).read())
         for i in data['interpreterSettings'].keys():
+            if data['interpreterSettings'][i]['group'] == 'md':
+                continue
             if r_enabled == 'true':
                 data['interpreterSettings'][i]['properties'].update(r_conf)
             data['interpreterSettings'][i]['interpreterGroup'] = groups
+
         if interpreter_mode != 'remote':
             with open(local_interpreters_config, 'w') as f:
                 f.write(json.dumps(data, indent=2))
             put(local_interpreters_config, local_interpreters_config)
+            sudo('cp -f {0} {1}'.format(local_interpreters_config, interpreters_config))
             sudo('systemctl restart zeppelin-notebook')
         else:
             with open(interpreters_config, 'w') as f:
