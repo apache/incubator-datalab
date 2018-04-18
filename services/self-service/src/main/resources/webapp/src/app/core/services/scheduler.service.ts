5e01582c0ae05601b5c3372d9f31f9a138301899
Andriana Kovalyshyn <Andriana_Kovalyshyn@epam.com>
2018-04-18 14:53:13 +0300

    [EPMCDLAB-1436]: catching error fixes

--- services/self-service/src/main/resources/webapp/src/app/core/services/scheduler.service.ts
+++ services/self-service/src/main/resources/webapp/src/app/core/services/scheduler.service.ts
@@ -31,14 +31,22 @@ export class SchedulerService {
     return this.applicationServiceFacade
       .buildGetExploratorySchedule(param)
       .map((response: Response) => response.json())
-      .catch((error: any) => error);
+      .catch((error: Response | any) => {
+        return Observable.throw(
+            new Error(`{"status": "${ error.status }", "statusText": "${ error.statusText }", "message": "${ error._body }"}`)
+        );
+    });
   }
 
-  public setExploratorySchedule(notebook, data, resource?): Observable<any> {
+  public setExploratorySchedule(notebook, data, resource?): Observable<Response> {
     const param = resource ? `/${notebook}/${resource}` : `/${notebook}`;
     return this.applicationServiceFacade
       .buildSetExploratorySchedule(param, data)
       .map((response: Response) => response)
-      .catch((error: any) => error);
+      .catch((error: Response | any) => {
+        return Observable.throw(
+            new Error(`{"status": "${ error.status }", "statusText": "${ error.statusText }", "message": "${ error._body }"}`)
+        );
+    });
   }
 }
