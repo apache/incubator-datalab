8556ea69156261e27058f095e1cf30ccd12f95b7
Denys_Shliakhov <denys_shliakhov@epam.com>
2018-04-19 15:39:01 +0300

    HotFix. Removed explicit version of azure SDK

--- infrastructure-provisioning/src/general/scripts/azure/ssn_configure.py
+++ infrastructure-provisioning/src/general/scripts/azure/ssn_configure.py
@@ -158,7 +158,7 @@ if __name__ == "__main__":
     try:
         logging.info('[INSTALLING PREREQUISITES TO SSN INSTANCE]')
         print('[INSTALLING PREREQUISITES TO SSN INSTANCE]')
-        params = "--hostname {} --keyfile {} --pip_packages 'argparse fabric pymongo pyyaml pycrypto azure==2.0.0' \
+        params = "--hostname {} --keyfile {} --pip_packages 'argparse fabric pymongo pyyaml pycrypto azure' \
             --user {} --region {}".format(ssn_conf['instance_dns_name'], ssn_conf['ssh_key_path'],
                                           ssn_conf['dlab_ssh_user'], ssn_conf['region'])
         try:
