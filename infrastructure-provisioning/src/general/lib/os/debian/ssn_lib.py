11a7cb579d19d4d3bc8de69a48a830509d7939a9
Denys_Shliakhov <denys_shliakhov@epam.com>
2018-05-29 16:58:21 +0300

    Fixed.Jenkins isn't installed on ssn node.Debian

--- infrastructure-provisioning/src/general/lib/os/debian/ssn_lib.py
+++ infrastructure-provisioning/src/general/lib/os/debian/ssn_lib.py
@@ -63,6 +63,7 @@ def ensure_jenkins(dlab_path):
             sudo('wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | apt-key add -')
             sudo('echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list')
             sudo('apt-get -y update')
+            sudo('apt-get -y install openjdk-8-jdk')
             sudo('apt-get -y install jenkins')
             sudo('touch ' + dlab_path + 'tmp/jenkins_ensured')
         return True
