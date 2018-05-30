ccac74d4b4eb78b2ee08318d26dfc5638039c0f3
Denys_Shliakhov <denys_shliakhov@epam.com>
2018-05-30 09:05:32 +0300

    [EPMCDLAB-1639]:Fixed.Start/Stop/Terminate Spark cluster with similar name.AWS

--- infrastructure-provisioning/src/general/scripts/aws/dataengine_start.py
+++ infrastructure-provisioning/src/general/scripts/aws/dataengine_start.py
@@ -30,15 +30,17 @@ import uuid
 def start_data_engine(cluster_name):
     print("Start Data Engine")
     try:
-        start_ec2('Name', cluster_name + '*')
+        start_ec2('user:tag', cluster_name)
     except:
         sys.exit(1)
 
 
 if __name__ == "__main__":
-    local_log_filename = "{}_{}_{}.log".format(os.environ['conf_resource'], os.environ['edge_user_name'],
+    local_log_filename = "{}_{}_{}.log".format(os.environ['conf_resource'],
+                                               os.environ['edge_user_name'],
                                                os.environ['request_id'])
-    local_log_filepath = "/logs/" + os.environ['conf_resource'] + "/" + local_log_filename
+    local_log_filepath = "/logs/" + \
+                         os.environ['conf_resource'] + "/" + local_log_filename
     logging.basicConfig(format='%(levelname)-8s [%(asctime)s]  %(message)s',
                         level=logging.DEBUG,
                         filename=local_log_filepath)
@@ -47,6 +49,7 @@ if __name__ == "__main__":
     create_aws_config_files()
     print('Generating infrastructure names and tags')
     data_engine_config = dict()
+    
     try:
         data_engine_config['exploratory_name'] = os.environ['exploratory_name']
     except:
@@ -58,13 +61,16 @@ if __name__ == "__main__":
     data_engine_config['service_base_name'] = os.environ['conf_service_base_name']
     data_engine_config['user_name'] = os.environ['edge_user_name']
     data_engine_config['cluster_name'] = \
-        data_engine_config['service_base_name'] + '-' + data_engine_config['user_name'] + '-de-' + \
-        data_engine_config['exploratory_name'] + '-' + data_engine_config['computational_name']
+        data_engine_config['service_base_name'] + '-' + \
+        data_engine_config['user_name'] + '-de-' + \
+        data_engine_config['exploratory_name'] + '-' + \
+        data_engine_config['computational_name']
 
     logging.info('[START DATA ENGINE CLUSTER]')
     print('[START DATA ENGINE CLUSTER]')
     try:
-        start_data_engine(data_engine_config['cluster_name'])
+        start_data_engine("{}:{}".format(os.environ['conf_service_base_name'],
+                                         data_engine_config['cluster_name']))
     except Exception as err:
         append_result("Failed to start Data Engine.", str(err))
         sys.exit(1)
