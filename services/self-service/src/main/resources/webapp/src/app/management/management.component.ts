f5189f846d198d54b103afc39decdae5020967a5
Andriana Kovalyshyn <Andriana_Kovalyshyn@epam.com>
2018-07-19 16:03:11 +0300

    [EPMCDLAB-1762]: added separated management page

---
+++ services/self-service/src/main/resources/webapp/src/app/management/management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'environments-management',
+  templateUrl: './management.component.html',
+  styleUrls: ['./management.component.scss']
+})
+export class ManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
