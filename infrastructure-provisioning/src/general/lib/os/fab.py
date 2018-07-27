c070dc2212aa1b1b68fdec7a6e0eeccaa20a9285
Yevhen Hrabovskyi <zgrabovskiy@gmail.com>
2018-07-27 14:48:53 +0300

    [REVERSE-PROXY]: Fix ungit. Fix emr 5.6 fail on start.

--- infrastructure-provisioning/src/general/lib/os/fab.py
+++ infrastructure-provisioning/src/general/lib/os/fab.py
@@ -340,11 +340,11 @@ def install_ungit(os_user, notebook_name):
             sudo('npm -g install ungit@{}'.format(os.environ['notebook_ungit_version']))
             put('/root/templates/ungit.service', '/tmp/ungit.service')
             sudo("sed -i 's|OS_USR|{}|' /tmp/ungit.service".format(os_user))
-            sudo("sed -i 's|NOTEBOOK_NAME|{}|' /tmp/ungit.service".format(
-                notebook_name))
             http_proxy = run('echo $http_proxy')
             sudo("sed -i 's|PROXY_HOST|{}|g' /tmp/ungit.service".format(http_proxy))
-            sudo('mv -f /tmp/ungit.service /etc/systemd/system/ungit.service')
+            sudo("cp -f /tmp/ungit.service /etc/systemd/system/ungit.service")
+            sudo("sed -i 's|NOTEBOOK_NAME|{}|' /etc/systemd/system/ungit.service".format(
+                notebook_name))
             run('git config --global user.name "Example User"')
             run('git config --global user.email "example@example.com"')
             run('mkdir -p ~/.git/templates/hooks')
@@ -364,7 +364,8 @@ def install_ungit(os_user, notebook_name):
             sys.exit(1)
     else:
         try:
-            sudo("sed -i 's|NOTEBOOK_NAME|{}|' /etc/systemd/system/ungit.service".format(
+            sudo('cp -f /tmp/ungit.service /etc/systemd/system/ungit.service')
+            sudo("sed -i 's||{}|' /etc/systemd/system/ungit.service".format(
                 notebook_name))
             sudo('systemctl daemon-reload')
             sudo('systemctl restart ungit.service')
