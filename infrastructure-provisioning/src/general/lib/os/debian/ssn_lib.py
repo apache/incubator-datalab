ab5cde3cf2642682f09ad494851a5dad3807f3fd
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2018-11-16 14:17:18 +0200

    changed folder name

--- infrastructure-provisioning/src/general/lib/os/debian/ssn_lib.py
+++ infrastructure-provisioning/src/general/lib/os/debian/ssn_lib.py
@@ -207,7 +207,7 @@ def start_ss(keyfile, host_string, dlab_conf_dir, web_path,
                     # sudo('cp {0}/web_app/{1}/*.jar {1}{2}/'.format(dlab_path, web_path, service))
                     jar = sudo('cd {0}{1}/lib/; find {1}*.jar -type f'.format(web_path, service))
                     sudo('ln -s {0}{2}/lib/{1} {0}{2}/{2}.jar '.format(web_path, jar, service))
-                    sudo('cp {0}/web_app/{1}/conf/*.yml /tmp/yml_tmp/'.format(dlab_path, service))
+                    sudo('cp {0}/webapp/{1}/conf/*.yml /tmp/yml_tmp/'.format(dlab_path, service))
                 if cloud_provider == 'azure':
                     for config in ['self-service', 'security']:
                         sudo('sed -i "s|<LOGIN_USE_LDAP>|{1}|g" /tmp/yml_tmp/{0}.yml'.format(config, ldap_login))
