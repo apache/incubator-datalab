a8170420501b5f97507478608c76435ad4d6e339
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2018-12-13 16:41:56 +0200

    added creation of jenkins repo

--- infrastructure-provisioning/scripts/deploy_repository/templates/configureNexus.groovy
+++ infrastructure-provisioning/scripts/deploy_repository/templates/configureNexus.groovy
@@ -23,10 +23,11 @@ repository.getRepositoryManager().delete('nuget.org-proxy');
 blobStore.createFileBlobStore('artifacts_store', 'artifacts_store')
 blobStore.createFileBlobStore('packages_store', 'packages_store')
 blobStore.createFileBlobStore('docker_store', 'docker_store')
-repository.createPyPiProxy('pypi','https://pypi.org/', 'packages_store', true)
+repository.createPyPiProxy('pypi-repo','https://pypi.org/', 'packages_store', true)
 repository.createMavenProxy('maven-central','https://repo1.maven.org/maven2/', 'artifacts_store', true, VersionPolicy.RELEASE, LayoutPolicy.PERMISSIVE)
 repository.createDockerProxy('docker-hub', 'https://registry-1.docker.io', 'HUB', null, null, null, 'docker_store', true, true)
-repository.createRawProxy('docker-rep','https://download.docker.com/linux/ubuntu', 'packages_store')
 repository.createDockerGroup('docker-group', null, 8082, ['docker-hub'], true, 'docker_store')
+repository.createRawProxy('docker-repo','https://download.docker.com/linux/ubuntu', 'packages_store')
+repository.createRawProxy('jenkins-repo','http://pkg.jenkins.io/debian-stable', 'packages_store')
 
 log.info('Script rawRepositories completed successfully')
