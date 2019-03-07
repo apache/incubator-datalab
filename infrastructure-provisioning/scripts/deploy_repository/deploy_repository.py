6d959b6f42b96ada3708c7518b01b71e1632e231
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2019-03-07 13:43:35 +0200

    added headers;

--- infrastructure-provisioning/scripts/deploy_repository/deploy_repository.py
+++ infrastructure-provisioning/scripts/deploy_repository/deploy_repository.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # *****************************************************************************
 #
-# Copyright (c) 2016, EPAM SYSTEMS INC
+# Copyright (c) 2019, EPAM SYSTEMS INC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- infrastructure-provisioning/scripts/deploy_repository/scripts/addUpdateScript.groovy
+++ infrastructure-provisioning/scripts/deploy_repository/scripts/addUpdateScript.groovy
@@ -1,6 +1,19 @@
-/**
- * An example script that handles adding or updating a groovy script via the REST API.
+/*
+ * Copyright (c) 2019, EPAM SYSTEMS INC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 @Grab('org.sonatype.nexus:nexus-rest-client:3.9.0-01')
 @Grab('org.sonatype.nexus:nexus-rest-jackson2:3.9.0-01')
 @Grab('org.sonatype.nexus:nexus-script:3.9.0-01')
