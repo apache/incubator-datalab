de83a8f0703f54631dc8370619e873b0e5244325
Dyoma33 <demian.mysakovets1@gmail.com>
2019-03-14 13:11:20 +0200

    Fixed data engine creation issue, jira ticket 328. Files from toree_kernel.tar.gz were placed in "/home/{user-name}/.local/share/jupyter/kernels/{cluster-name}/toree-0.2.0-incubating/", and should be directly in cluster folder.

--- infrastructure-provisioning/src/general/scripts/os/jupyter_dataengine_create_configs.py
+++ infrastructure-provisioning/src/general/scripts/os/jupyter_dataengine_create_configs.py
@@ -82,6 +82,8 @@ def toree_kernel(args):
     scala_version = local('scala -e "println(scala.util.Properties.versionNumberString)"', capture=True)
     local('mkdir -p ' + kernels_dir + 'toree_' + args.cluster_name + '/')
     local('tar zxvf /tmp/{}/toree_kernel.tar.gz -C '.format(args.cluster_name) + kernels_dir + 'toree_' + args.cluster_name + '/')
+    local('sudo mv {0}toree_{1}/toree-0.2.0-incubating/* {0}toree_{1}/'.format(kernels_dir, args.cluster_name))
+    local('sudo rm -r {0}toree_{1}/toree-0.2.0-incubating'.format(kernels_dir, args.cluster_name))
     kernel_path = kernels_dir + "toree_" + args.cluster_name + "/kernel.json"
     template_file = "/tmp/{}/toree_dataengine_template.json".format(args.cluster_name)
     with open(template_file, 'r') as f:
