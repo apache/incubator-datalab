4fe05f7a45aaeae3586fade3b83df72043ee475c
i.oleksandr <i.oleksandr@gmail.com>
2019-05-13 17:45:00 +0300

    Update configure_docker.py

--- infrastructure-provisioning/src/ssn/scripts/configure_docker.py
+++ infrastructure-provisioning/src/ssn/scripts/configure_docker.py
@@ -122,8 +122,8 @@ def configure_guacamole():
         sudo('docker exec -i guac-mysql /bin/bash -c "cat /tmp/scripts/initdb.sql | mysql -u root -p{} guacamole"'.format(mysql_pass))
         sudo("docker run --name guacamole --restart unless-stopped --link guacd:guacd --link guac-mysql:mysql -e MYSQL_DATABASE='guacamole' -e MYSQL_USER='guacamole' -e MYSQL_PASSWORD='guacamole' -d -p 8080:8080 guacamole/guacamole")
         #create cronjob for run containers on reboot
-        sudo('touch /opt/dlab/handlers/mysql-cron.sh')
-        sudo('chmod 755 /opt/dlab/handlers/mysql-cron.sh')
+        sudo('touch /opt/dlab/mysql-cron.sh')
+        sudo('chmod 755 /opt/dlab/mysql-cron.sh')
     except Exception as err:
         traceback.print_exc()
         print('Failed to configure guacamole: ', str(err))
