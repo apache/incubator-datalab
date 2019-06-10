ed8e86afce07f929d9fe30d8ba3d25bf435f75ac
Andriana Kovalyshyn <Andriana_Kovalyshyn@epam.com>
2019-06-10 17:21:14 +0300

    [DLAB-743]: added empty list placeholder

--- services/self-service/src/main/resources/webapp/src/app/administration/administration.module.ts
+++ services/self-service/src/main/resources/webapp/src/app/administration/administration.module.ts
@@ -24,6 +24,7 @@ import { ManagenementModule } from './management';
 import { ProjectModule } from './project';
 import { RolesModule } from './roles';
 
+
 @NgModule({
   imports: [CommonModule, ManagenementModule, ProjectModule, RolesModule],
   declarations: [],
