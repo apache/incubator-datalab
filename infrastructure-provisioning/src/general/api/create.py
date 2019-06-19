381d6560c13f16c71f176a2e9060acff5b07d27a
Mykola Bodnar1 <Mykola_Bodnar1@epam.com>
2019-06-19 15:48:25 +0300

    project creatin (subnet creation) (/project_prepare.py fixed; create.py fixed)

--- infrastructure-provisioning/src/general/api/create.py
+++ infrastructure-provisioning/src/general/api/create.py
@@ -56,10 +56,10 @@ if __name__ == "__main__":
             response_file.write(json.dumps(reply))
     else:
         reply['response']['log'] = "/var/log/dlab/{0}/{0}_{1}_{2}.log".format(os.environ['conf_resource'],
-                                                                              os.environ['edge_user_name'],
+                                                                              os.environ['project_tag'],
                                                                               os.environ['request_id'])
 
-        with open("/response/{}_{}_{}.json".format(os.environ['conf_resource'], os.environ['edge_user_name'],
+        with open("/response/{}_{}_{}.json".format(os.environ['conf_resource'], os.environ['project_tag'],
                                                    os.environ['request_id']), 'w') as response_file:
             response_file.write(json.dumps(reply))
 
--- infrastructure-provisioning/src/general/scripts/aws/project_prepare.py
+++ infrastructure-provisioning/src/general/scripts/aws/project_prepare.py
@@ -41,7 +41,6 @@ if __name__ == "__main__":
     project_conf['service_base_name'] = os.environ['conf_service_base_name']
     project_conf['project_tag_value'] = os.environ['project_tag']
     project_conf['vpc_id'] = os.environ['aws_vpc_id']
-    project_conf['vpc2_id'] = os.environ['aws_notebook_vpc_id']
     project_conf['region'] = os.environ['aws_region']
     project_conf['zone'] = os.environ['aws_region'] + os.environ['aws_zone']
     project_conf['tag_name'] = '{}-Tag'.format(project_conf['service_base_name'])
@@ -49,7 +48,40 @@ if __name__ == "__main__":
     project_conf['private_subnet_prefix'] = os.environ['aws_private_subnet_prefix']
     project_conf['private_subnet_name'] = '{0}-{1}-subnet'.format(project_conf['service_base_name'], project_conf['project_tag_value'])
 
+    project_conf['vpc2_cidr'] = os.environ['conf_vpc2_cidr']
+    project_conf['tag2_name'] = project_conf['service_base_name'] + '-secondary-Tag'
+    project_conf['vpc2_name'] = '{}-secondary-VPC'.format(project_conf['service_base_name'])
 
+    try:
+        if os.environ['conf_duo_vpc_enable'] == 'true' and not os.environ['aws_vpc2_id']:
+            try:
+                pre_defined_vpc2 = True
+                logging.info('[CREATE SECONDARY VPC AND ROUTE TABLE]')
+                print('[CREATE SECONDARY VPC AND ROUTE TABLE]')
+                params = "--vpc {} --region {} --infra_tag_name {} --infra_tag_value {} --secondary " \
+                         "--vpc_name {}".format(project_conf['vpc2_cidr'], project_conf['region'], project_conf['tag2_name'], project_conf['service_base_name'], project_conf['vpc2_name'])
+                try:
+                    local("~/scripts/{}.py {}".format('ssn_create_vpc', params))
+                except:
+                    traceback.print_exc()
+                    raise Exception
+                os.environ['aws_vpc2_id'] = get_vpc_by_tag(tag2_name, service_base_name)
+            except Exception as err:
+                print('Error: {0}'.format(err))
+                append_result("Failed to create secondary VPC. Exception:" + str(err))
+                if pre_defined_vpc:
+                    remove_internet_gateways(os.environ['aws_vpc_id'], tag_name, service_base_name)
+                    remove_route_tables(tag_name, True)
+                    remove_vpc(os.environ['aws_vpc_id'])
+                sys.exit(1)
+        else:
+            try:
+                 project_conf['vpc2_id'] = os.environ['aws_notebook_vpc_id']
+            except KeyError:
+                project_conf['vpc2_id'] = project_conf['vpc_id']
+    except:
+        traceback.print_exc()
+        sys.exit(1)
 
     try:
         if os.environ['conf_user_subnets_range'] == '':
@@ -71,7 +103,6 @@ if __name__ == "__main__":
             project_conf['private_subnet_prefix'], os.environ['conf_user_subnets_range'], project_conf['private_subnet_name'],
             project_conf['zone'])
         try:
-            create_rt(project_conf['vpc2_id'], project_conf['tag_name'], project_conf['service_base_name'], os.environ['secondary'])
             local("~/scripts/{}.py {}".format('common_create_subnet', params))
         except:
             traceback.print_exc()
@@ -81,6 +112,10 @@ if __name__ == "__main__":
         append_result("Failed to create subnet.", str(err))
         sys.exit(1)
 
-    tag = {"Key": project_conf['tag_name'], "Value": "{0}-{1}-subnet".format(project_conf['service_base_name'], project_conf['project_name'])}
+    tag = {"Key": project_conf['tag_name'], "Value": "{0}-{1}-subnet".format(project_conf['service_base_name'], project_conf['project_tag_value'])}
     project_conf['private_subnet_cidr'] = get_subnet_by_tag(tag)
+    project_tag = {"Key": 'project_tag', "Value": project_conf['project_tag_value']}
+    subnet_id = get_subnet_by_cidr(project_conf['private_subnet_cidr'])
+    print('subnet id: {}'.format(subnet_id))
+    create_tag(subnet_id, project_tag)
     print('NEW SUBNET CIDR CREATED: {}'.format(project_conf['private_subnet_cidr']))
