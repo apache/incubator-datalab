511ce8845ded84504649db4ba786f7aaf3da7f68
Andriana Kovalyshyn <Andriana_Kovalyshyn@epam.com>
2019-06-27 12:44:08 +0300

    [DLAB-823]: added placeholder to billing grid

--- services/self-service/src/main/resources/webapp/src/app/reporting/reporting-grid/reporting-grid.component.ts
+++ services/self-service/src/main/resources/webapp/src/app/reporting/reporting-grid/reporting-grid.component.ts
@@ -25,7 +25,7 @@ import { DICTIONARY, ReportingConfigModel } from '../../../dictionary/global.dic
   selector: 'dlab-reporting-grid',
   templateUrl: './reporting-grid.component.html',
   styleUrls: ['./reporting-grid.component.scss',
-              '../../resources/resources-grid/resources-grid.component.css']
+    '../../resources/resources-grid/resources-grid.component.css']
 })
 export class ReportingGridComponent implements OnInit {
   readonly DICTIONARY = DICTIONARY;
@@ -41,15 +41,15 @@ export class ReportingGridComponent implements OnInit {
   displayedColumns: string[] = ['name', 'user', 'type', 'status', 'shape', 'service', 'charge'];
   displayedFilterColumns: string[] = ['name-filter', 'user-filter', 'type-filter', 'status-filter', 'shape-filter', 'service-filter', 'actions'];
 
-  public filteringColumns: Array<any> = [
-    { title: 'User', name: 'user', className: 'th_user', filtering: true, role: 'admin'},
-    { title: 'Environment name', name: 'dlab_id', className: 'env_name', filtering: true },
-    { title: 'Resource Type', name: 'resource_type', className: 'th_type', filtering: true },
-    { title: 'Status', name: 'status', className: 'th_rstatus', filtering: true },
-    { title: DICTIONARY.instance_size, name: DICTIONARY.billing.instance_size, className: 'th_shape', filtering: true },
-    { title: DICTIONARY.service, name: DICTIONARY.billing.service_filter_key, className: 'service', filtering: true },
-    { title: 'Service Charges', name: 'charges', className: 'th_charges', filtering: false }
-  ];
+  // public filteringColumns: Array<any> = [
+  //   { title: 'User', name: 'user', className: 'th_user', filtering: true, role: 'admin'},
+  //   { title: 'Environment name', name: 'dlab_id', className: 'env_name', filtering: true },
+  //   { title: 'Resource Type', name: 'resource_type', className: 'th_type', filtering: true },
+  //   { title: 'Status', name: 'status', className: 'th_rstatus', filtering: true },
+  //   { title: DICTIONARY.instance_size, name: DICTIONARY.billing.instance_size, className: 'th_shape', filtering: true },
+  //   { title: DICTIONARY.service, name: DICTIONARY.billing.service_filter_key, className: 'service', filtering: true },
+  //   { title: 'Service Charges', name: 'charges', className: 'th_charges', filtering: false }
+  // ];
 
   ngOnInit() { }
 
