7733f9665cc05adf4ee3772109b549a1dc13686e
Oleksandr_Isniuk <i.oleksandr@gmail.com>
2019-07-04 17:58:16 +0300

    [DLAB-759] TF module for Mongo-HA [DLAB-836] TF module for nginx load balancer [DLAB-811] chart for dlab-ui

---
+++ infrastructure-provisioning/terraform/aws/modules/nginx/nginx.tf
@@ -0,0 +1,48 @@
+# *****************************************************************************
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# ******************************************************************************
+provider "helm" {
+    install_tiller  = true
+    namespace       = "kube-system"
+    service_account = "tiller"
+    tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.14.1"
+}
+
+
+resource "helm_release" "my_mongo" {
+    name      = "nginx-ingress"
+    chart     = "stable/nginx-ingress"
+
+    set {
+        name = "controller.service.nodePorts.http"
+        value = "${var.mongo_root_pwd}"
+    }
+
+    set {
+        name = "controller.service.nodePorts.https"
+        value = "${var.mongo_db_username}"
+    }
+
+    set {
+        name = "controller.service.type"
+        value = "NodePort"
+    }
+
+}
