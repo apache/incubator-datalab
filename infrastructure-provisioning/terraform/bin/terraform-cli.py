2e8b32298f3cc16d4f6a4614b9521bab886e5831
bohdana_kuzmenko <bohdana.kuzmenko.work@gmail.com>
2019-07-09 12:00:08 +0300

    Create separate class for terraform Update cli parameters initialization

--- infrastructure-provisioning/terraform/bin/terraform-cli.py
+++ infrastructure-provisioning/terraform/bin/terraform-cli.py
@@ -5,6 +5,54 @@ import abc
 import argparse
 
 
+class TerraformProviderError(Exception):
+    """
+    Raises errors while terraform provision
+    """
+    pass
+
+
+class TerraformProvider:
+    def initialize(self):
+        """Initialize terraform
+
+        Returns:
+             bool: init successful
+        """
+        terraform_success_init = 'Terraform has been successfully initialized!'
+        terraform_init_result = self.console_execute('terraform init')
+        if terraform_success_init not in terraform_init_result:
+            raise TerraformProviderError(terraform_init_result)
+
+    def validate(self):
+        """Validate terraform
+
+        Returns:
+             bool: validation successful
+        """
+        terraform_success_validate = 'Success! The configuration is valid.'
+        terraform_validate_result = self.console_execute('terraform validate')
+        if terraform_success_validate not in terraform_validate_result:
+            raise TerraformProviderError(terraform_validate_result)
+
+    def apply(self, cli_args):
+        """Run terraform
+
+        Args:
+            cli_args: dict of parameters
+        Returns:
+             None
+        """
+        args = ['-var {0}={1}'.format(key, value) for key, value
+                in cli_args.items() if value]
+        args_str = ' '.join(args)
+        print('terraform apply {}'.format(args_str))
+
+    @staticmethod
+    def console_execute(command):
+        return os.popen(command).read()
+
+
 class AbstractDeployBuilder:
 
     @property
@@ -29,7 +77,7 @@ class AbstractDeployBuilder:
         raise NotImplementedError
 
     @abc.abstractmethod
-    def install(self):
+    def deploy(self):
         """Post terraform execution
 
         Returns:
@@ -44,44 +92,11 @@ class AbstractDeployBuilder:
             dict: CLI arguments
         """
         parser = argparse.ArgumentParser()
-        for argument, props in self.cli_args.items():
-            parser.add_argument(argument, **props)
+        for argument in self.cli_args:
+            parser.add_argument(argument.get('name'), **argument.get('props'))
         return vars(parser.parse_args())
 
-    def tf_init(self):
-        """Initialize terraform
-
-        Returns:
-             bool: init successful
-        """
-        terraform_success_init = 'Terraform has been successfully initialized!'
-        terraform_init_result = self.console_execute('terraform init')
-        return terraform_success_init in terraform_init_result
-
-    def tf_validate(self):
-        """Validate terraform
-
-        Returns:
-             bool: validation successful
-        """
-        terraform_success_validate = 'Success! The configuration is valid.'
-        terraform_validate_result = self.console_execute('terraform validate')
-        return terraform_success_validate in terraform_validate_result
-
-    def tf_apply(self, cli_args):
-        """Run terraform
-
-        Args:
-            cli_args: dict of parameters
-        Returns:
-             None
-        """
-        args = ['-var {0}={1}'.format(key, value) for key, value
-                in cli_args.items() if value]
-        args_str = ' '.join(args)
-        print('terraform apply {}'.format(args_str))
-
-    def run_tf(self):
+    def provision(self):
         """Execute terraform script
 
         Returns:
@@ -89,16 +104,34 @@ class AbstractDeployBuilder:
         """
         tf_location = self.terraform_location
         cli_args = self.parse_args()
+        terraform = TerraformProvider()
 
         os.chdir(tf_location)
+        try:
+            terraform.initialize()
+            terraform.validate()
+            terraform.apply(cli_args)
+        except TerraformProviderError as error:
+            print(error)
 
-        if self.tf_init() and self.tf_validate():
-            self.tf_apply(cli_args)
+    def build_str_arg_param(self, name, desc, **kwargs):
+        return self.build_arg_param(str, name, desc, **kwargs)
 
+    def build_int_arg_param(self, name, desc, **kwargs):
+        return self.build_arg_param(int, name, desc, **kwargs)
 
     @staticmethod
-    def console_execute(command):
-        return os.popen(command).read()
+    def build_arg_param(arg_type, name, desc, **kwargs):
+        return {
+            'name': name,
+            'props': {
+                'help': desc,
+                'type': arg_type,
+                'nargs': kwargs.get('nargs', '?'),
+                'default': kwargs.get('default'),
+                'choices': kwargs.get('choices'),
+            }
+        }
 
 
 class DeployDirector:
@@ -111,8 +144,8 @@ class DeployDirector:
         Returns:
             None
         """
-        builder.run_tf()
-        builder.install()
+        builder.provision()
+        builder.deploy()
 
     def get_status(self):
         """ Get execution status
@@ -132,110 +165,55 @@ class AWSSourceBuilder(AbstractDeployBuilder):
 
     @property
     def cli_args(self):
-        return {
-            'service_base_name': {
-                'type': str,
-                'help': 'Any infrastructure value (should be unique '
-                        'if multiple SSN’s have been deployed before).',
-                'nargs': '?',
-                'default': 'dlab-k8s',
-            },
-            'vpc_id': {
-                'type': str,
-                'nargs': '?',
-                'help': 'ID of AWS VPC if you already have VPC created.',
-            },
-            'vpc_cidr': {
-                'type': str,
-                'help': 'CIDR for VPC creation. Conflicts with vpc_id',
-                'nargs': '?',
-                'default': '172.31.0.0/16',
-            },
-            'subnet_id': {
-                'type': str,
-                'nargs': '?',
-                'help': 'ID of AWS Subnet if you already have subnet created.',
-            },
-            'subnet_cidr': {
-                'type': str,
-                'help': 'CIDR for Subnet creation. Conflicts with subnet_id.',
-                'nargs': '?',
-                'default': '172.31.0.0/24',
-            },
-            'env_os': {
-                'type': str,
-                'help': 'OS type. Available options: debian, redhat.',
-                'nargs': '?',
-                'default': 'debian',
-                'choices': ('debian', 'redhat'),
-            },
-            'ami': {  # from python
-                'type': str,
-                'help': 'ID of EC2 AMI.',
-                'nargs': '?',
-            },
-            'key_name': {  # from python
-                'type': str,
-                'help': 'Name of EC2 Key pair.',
-                'nargs': '?',
-            },
-            'region': {
-                'type': str,
-                'help': 'Name of AWS region.',
-                'nargs': '?',
-                'default': 'us-west-2',
-            },
-            'zone': {
-                'type': str,
-                'help': 'Name of AWS zone',
-                'nargs': '?',
-                'default': 'a',
-            },
-            'ssn_k8s_masters_count': {
-                'type': int,
-                'help': 'Count of K8S masters.',
-                'nargs': '?',
-                'default': 3,
-            },
-            'ssn_k8s_workers_count': {
-                'type': int,
-                'help': 'Count of K8S workers',
-                'nargs': '?',
-                'default': 2,
-            },
-            'ssn_root_volume_size': {
-                'type': int,
-                'help': 'Size of root volume in GB.',
-                'nargs': '?',
-                'default': 30,
-            },
-            'allowed_cidrs': {
-                'type': str,
-                'help': 'CIDR to allow acces to SSN K8S cluster.',
-                'nargs': '?',
-                'default': '0.0.0.0/0',
-            },
-            'ssn_k8s_masters_shape': {
-                'type': str,
-                'help': 'Shape for SSN K8S masters.',
-                'nargs': '?',
-                'default': 't2.medium',
-            },
-            'ssn_k8s_workers_shape': {
-                'type': str,
-                'help': 'Shape for SSN K8S workers.',
-                'nargs': '?',
-                'default': 't2.medium',
-            },
-            'os_user': {
-                'type': str,
-                'help': 'Name of DLab service user.',
-                'nargs': '?',
-                'default': 'dlab-user',
-            },
-        }
+        return [
+            self.build_str_arg_param('service_base_name',
+                                     'Any infrastructure value (should be '
+                                     'unique if multiple SSN\'s have been '
+                                     'deployed before).',
+                                     default='dlab-k8s'),
+            self.build_str_arg_param('vpc_id',
+                                     'ID of AWS VPC if you already have VPC '
+                                     'created.'),
+            self.build_str_arg_param('vpc_cidr',
+                                     'CIDR for VPC creation. '
+                                     'Conflicts with vpc_id',
+                                     default='172.31.0.0/16'),
+            self.build_str_arg_param('subnet_id',
+                                     'ID of AWS Subnet if you already have '
+                                     'subnet created.'),
+            self.build_str_arg_param('subnet_cidr',
+                                     'CIDR for Subnet creation. Conflicts with '
+                                     'subnet_id.',
+                                     default='172.31.0.0/24'),
+            self.build_str_arg_param('env_os',
+                                     'OS type.',
+                                     default='debian',
+                                     choices=('debian', 'redhat')),
+            self.build_str_arg_param('ami', 'ID of EC2 AMI.'),
+            self.build_str_arg_param('key_name', 'Name of EC2 Key pair.'),
+            self.build_str_arg_param('region', 'Name of AWS region.',
+                                     default='us-west-2'),
+            self.build_str_arg_param('zone', 'Name of AWS zone', default='a'),
+            self.build_str_arg_param('allowed_cidrs',
+                                     'CIDR to allow acces to SSN K8S cluster.',
+                                     default='0.0.0.0/0'),
+            self.build_str_arg_param('ssn_k8s_masters_shape',
+                                     'Shape for SSN K8S masters.',
+                                     default='t2.medium'),
+            self.build_str_arg_param('ssn_k8s_workers_shape',
+                                     'Shape for SSN K8S workers.',
+                                     default='t2.medium'),
+            self.build_str_arg_param('os_user', 'Name of DLab service user.',
+                                     default='dlab-user'),
+            self.build_int_arg_param('ssn_k8s_masters_count',
+                                     'Count of K8S masters.', default=3),
+            self.build_int_arg_param('ssn_k8s_workers_count',
+                                     'Count of K8S workers', default=2),
+            self.build_int_arg_param('ssn_root_volume_size',
+                                     'Size of root volume in GB.', default=30),
+        ]
 
-    def install(self):
+    def deploy(self):
         # os.system('ls -l')
         print('installation process')
 
