b1fe14ed4aa90edb5f2d2a363baca308c3d3454f
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2019-07-16 12:52:34 +0300

    added outputs

--- infrastructure-provisioning/terraform/aws/ssn-k8s/main/main.tf
+++ infrastructure-provisioning/terraform/aws/ssn-k8s/main/main.tf
@@ -24,3 +24,28 @@ provider "aws" {
   access_key = var.access_key_id
   secret_key = var.secret_access_key
 }
+
+output "ssn_k8s_alb_dns_name" {
+  value = aws_lb.ssn_k8s_alb.dns_name
+}
+
+output "ssn_k8s_masters_ip_addresses" {
+  value = data.aws_instances.ssn_k8s_masters_instances.public_ips
+  depends_on = [data.aws_instances.ssn_k8s_masters_instances]
+}
+
+output "ssn_bucket_name" {
+  value = aws_s3_bucket.ssn_k8s_bucket.id
+}
+
+output "ssn_vpc_id" {
+  value = data.aws_vpc.ssn_k8s_vpc_data.id
+}
+
+output "ssn_subnets" {
+  value = compact([data.aws_subnet.k8s-subnet-a-data.id, data.aws_subnet.k8s-subnet-b-data.id, local.subnet_c_id])
+}
+
+output "ssn_k8s_sg_id" {
+  value = aws_security_group.ssn_k8s_sg.id
+}
