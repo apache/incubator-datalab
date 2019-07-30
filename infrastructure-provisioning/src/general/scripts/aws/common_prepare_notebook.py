a26f9b75da625020fb496629fa0da0c623cf212b
Mykola_Bodnar1 <bodnarmykola@gmail.com>
2019-07-30 11:04:01 +0300

    DLAB-865 Notebook creation from custom image fixed

--- infrastructure-provisioning/src/general/scripts/aws/common_prepare_notebook.py
+++ infrastructure-provisioning/src/general/scripts/aws/common_prepare_notebook.py
@@ -75,7 +75,11 @@ if __name__ == "__main__":
 
     notebook_config['expected_image_name'] = '{}-{}-notebook-image'.format(notebook_config['service_base_name'],
                                                                            os.environ['application'])
-    notebook_config['notebook_image_name'] = (lambda x: os.environ['notebook_image_name'] if x != 'None'
+    notebook_config['custom_image_name'] = '{0}-{1}-{2}-{3}'.format(notebook_config['service_base_name'],
+                                                                           os.environ['project_name'],
+                                                                           os.environ['application']
+                                                                           os.environ['notebook_image_name'])
+    notebook_config['notebook_image_name'] = (lambda x: notebook_config['custom_image_name'] if (x != None and x != '')
         else notebook_config['expected_image_name'])(str(os.environ.get('notebook_image_name')))
     print('Searching pre-configured images')
     notebook_config['ami_id'] = get_ami_id(os.environ['aws_{}_image_name'.format(os.environ['conf_os_family'])])
