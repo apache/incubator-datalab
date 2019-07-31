30a2f99f653206abfbca50ed13d0a45595519fdb
bhliva <bohdan_hliva@epam.com>
2019-07-31 16:21:02 +0300

    DLAB-000 added keycloak api

--- services/self-service/src/main/java/com/epam/dlab/backendapi/resources/KeycloakResource.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/resources/KeycloakResource.java
@@ -1,16 +1,18 @@
 package com.epam.dlab.backendapi.resources;
 
+import com.epam.dlab.auth.UserInfo;
 import com.epam.dlab.backendapi.service.SecurityService;
 import com.google.inject.Inject;
+import io.dropwizard.auth.Auth;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/test")
+@Path("/oauth")
 public class KeycloakResource {
 
 
@@ -21,9 +23,15 @@ public class KeycloakResource {
 		this.securityService = securityService;
 	}
 
-	@GET
+	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getUser(@QueryParam("code") String code) {
 		return Response.ok(securityService.getUserInfo(code)).build();
+	}
+
+	@POST
+	@Path("/authorize")
+	public Response authorize(@Auth UserInfo userInfo) {
+		return Response.ok().build();
 	}
 }
