a8f1ca17a317707cd9bf4a4d62f4e0c6edecaf75
bhliva <bohdan_hliva@epam.com>
2019-08-07 12:19:58 +0300

    DLAB-757 added API for getting keycloak login page

--- services/self-service/src/main/java/com/epam/dlab/backendapi/resources/KeycloakResource.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/resources/KeycloakResource.java
@@ -1,26 +1,39 @@
 package com.epam.dlab.backendapi.resources;
 
 import com.epam.dlab.auth.UserInfo;
+import com.epam.dlab.backendapi.SelfServiceApplicationConfiguration;
 import com.epam.dlab.backendapi.service.SecurityService;
 import com.google.inject.Inject;
 import io.dropwizard.auth.Auth;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Path("/oauth")
 public class KeycloakResource {
-
-
+	private static final String LOGIN_URI_FORMAT = "%s/realms/%s/protocol/openid-connect/auth?client_id=%s" +
+			"&response_type=code";
 	private final SecurityService securityService;
+	private final String loginUri;
 
 	@Inject
-	public KeycloakResource(SecurityService securityService) {
+	public KeycloakResource(SecurityService securityService, SelfServiceApplicationConfiguration configuration) {
 		this.securityService = securityService;
+		loginUri =
+				String.format(LOGIN_URI_FORMAT,
+						configuration.getKeycloakConfiguration().getAuthServerUrl(),
+						configuration.getKeycloakConfiguration().getRealm(),
+						configuration.getKeycloakConfiguration().getResource());
+	}
+
+	@GET
+	public Response getLoginUri() throws URISyntaxException {
+		return Response.noContent()
+				.location(new URI(loginUri))
+				.build();
 	}
 
 	@POST
