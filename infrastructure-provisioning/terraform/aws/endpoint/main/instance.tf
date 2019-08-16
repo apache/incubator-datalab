2cafa963f9612c7aa326ca6aa725551c04d3c842
AdamsDisturber <adamsdisturber@gmail.com>
2019-08-16 17:15:38 +0300

    Remove for later adding "endpoint_id" tag for ec2.

--- infrastructure-provisioning/terraform/aws/endpoint/main/instance.tf
+++ infrastructure-provisioning/terraform/aws/endpoint/main/instance.tf
@@ -40,7 +40,6 @@ resource "aws_instance" "endpoint" {
     "${var.service_base_name}-Tag" = "${local.ec2_name}"
     product = "${var.product}"
     "user:tag" = "${var.service_base_name}:${local.ec2_name}"
-    endpoint_id = "${var.endpoint_id}"
   }
 }
 
