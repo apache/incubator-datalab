efb3f2f4d69a8961c79480c9b0986b8076b88571
Andriana Kovalyshyn <Andriana_Kovalyshyn@epam.com>
2019-08-29 14:42:01 +0300

    [DLAB-1005]: prevent special characters on project name creation

--- services/self-service/src/main/resources/webapp/src/app/core/util/patterns.ts
+++ services/self-service/src/main/resources/webapp/src/app/core/util/patterns.ts
@@ -19,7 +19,7 @@
 
 export const PATTERNS = {
   namePattern: '[-_a-zA-Z0-9]*[_-]*[a-zA-Z0-9]+',
-  // noDoubleDush: '(([-_A-Za-z0-9])(?!\2))+',
+  projectName: '[a-zA-Z0-9]+',
   delimitersRegex: '/[-_]?/g',
   url: '[a-zA-Z0-9.://%#&\\.@:%-_\+~#=]*\.[^\s]*[a-zA-Z0-9]+',
   nodeCountPattern: '^[1-9]\\d*$',
