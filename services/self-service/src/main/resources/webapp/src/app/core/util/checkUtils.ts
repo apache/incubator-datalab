5443d069aa684fbd1040b236a73898c28c2ff549
Andriana Kovalyshyn <Andriana_Kovalyshyn@epam.com>
2019-09-05 16:40:47 +0300

    [DLAB-984]: added error message in case of project starting issues

--- services/self-service/src/main/resources/webapp/src/app/core/util/checkUtils.ts
+++ services/self-service/src/main/resources/webapp/src/app/core/util/checkUtils.ts
@@ -33,7 +33,7 @@ export class CheckUtils {
     const charCode = ($event.which) ? $event.which : $event.keyCode;
     if (charCode > 31 && (charCode < 48 || charCode > 57)
       && (charCode < 96 || charCode > 105)
-      || charCode === 45 || charCode === 47) {
+      || charCode === 45 || charCode === 47 || charCode === 101) {
       $event.preventDefault();
       return false;
     }
