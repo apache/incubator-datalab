4a4a4a8b0282518e13258762c69a55c1e482ea9d
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2019-09-10 12:43:20 +0300

    testing output

--- infrastructure-provisioning/terraform/gcp/ssn-gke/main/gke.tf
+++ infrastructure-provisioning/terraform/gcp/ssn-gke/main/gke.tf
@@ -58,6 +58,7 @@ resource "google_container_node_pool" "ssn_k8s_gke_node_pool" {
   node_count = var.ssn_k8s_workers_count
   version    = var.gke_cluster_version
   depends_on = [google_container_cluster.ssn_k8s_gke_cluster]
+  enable_legacy_abac = true
 
   node_config {
     machine_type = var.ssn_k8s_workers_shape
