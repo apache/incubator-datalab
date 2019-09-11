8320e93b075c7ba8b9562c9f85713cff8f45c676
AdamsDisturber <adamsdisturber@gmail.com>
2019-09-11 17:25:56 +0300

    Fixed MongoDB password parsing.

--- infrastructure-provisioning/src/ssn/scripts/configure_billing.py
+++ infrastructure-provisioning/src/ssn/scripts/configure_billing.py
@@ -100,7 +100,7 @@ def yml_billing(path):
             config_orig = config_orig.replace('<MONGODB_PASSWORD>', args.mongo_password)
         elif args.cloud_provider == 'gcp':
             config_orig = config_orig.replace('<CONF_SERVICE_BASE_NAME>', args.infrastructure_tag)
-            config_orig = config_orig.replace('<MONGODB_PASSWORD>', args.mongo_password)
+            config_orig = config_orig.replace('<MONGO_PASSWORD>', args.mongo_password)
             config_orig = config_orig.replace('<BILLING_DATASET_NAME>', args.billing_dataset_name)
         f = open(path, 'w')
         f.write(config_orig)
