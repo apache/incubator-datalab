b0a55998a9bee72a576f8cfd03a63fc163ea0fd6
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2019-09-11 13:47:59 +0300

    fixed typo

---
+++ services/self-service/entrypoint_gcp.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir -p /root/keys
+sudo keytool -genkeypair -alias dlab -keyalg RSA -validity 730 -storepass password \
+  -keypass password -keystore /root/keys/ssn.keystore.jks \
+  -keysize 2048 -dname "CN=localhost"
+sudo keytool -exportcert -alias dlab -storepass password -file /root/keys/ssn.crt \
+  -keystore /root/keys/ssn.keystore.jks
+
+/usr/bin/java -Xmx1024M -jar -Duser.timezone=UTC -Dfile.encoding=UTF-8 -DDLAB_CONF_DIR=/root/ /root/self-service-2.1.jar server /root/self-service.yml
