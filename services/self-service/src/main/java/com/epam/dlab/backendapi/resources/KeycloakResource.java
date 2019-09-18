dfa74fa0d78ac8d47403843edcaca06041b68c70
Oleh Fuks <olegfuks98@gmail.com>
2019-09-18 15:31:22 +0300

    [DLAB-1018]:  Added possibility to logout

--- services/self-service/src/main/java/com/epam/dlab/backendapi/resources/KeycloakResource.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/resources/KeycloakResource.java
@@ -6,7 +6,9 @@ import com.epam.dlab.backendapi.service.SecurityService;
 import com.google.inject.Inject;
 import io.dropwizard.auth.Auth;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
@@ -16,8 +18,10 @@ import java.net.URISyntaxException;
 public class KeycloakResource {
 	private static final String LOGIN_URI_FORMAT = "%s/realms/%s/protocol/openid-connect/auth?client_id=%s" +
 			"&response_type=code";
+	private static final String LOGOUT_URI_FORMAT = "%s/realms/%s/protocol/openid-connect/logout?redirect_uri=";
 	private final SecurityService securityService;
 	private final String loginUri;
+	private final String logoutUri;
 
 	@Inject
 	public KeycloakResource(SecurityService securityService, SelfServiceApplicationConfiguration configuration) {
@@ -27,6 +31,10 @@ public class KeycloakResource {
 						configuration.getKeycloakConfiguration().getAuthServerUrl(),
 						configuration.getKeycloakConfiguration().getRealm(),
 						configuration.getKeycloakConfiguration().getResource());
+		logoutUri =
+				String.format(LOGOUT_URI_FORMAT,
+						configuration.getKeycloakConfiguration().getAuthServerUrl(),
+						configuration.getKeycloakConfiguration().getRealm());
 	}
 
 	@GET
@@ -46,5 +54,18 @@ public class KeycloakResource {
 	@Path("/authorize")
 	public Response authorize(@Auth UserInfo userInfo) {
 		return Response.ok().build();
+	}
+
+	@GET
+	@Path("/logout")
+	public Response logout(final @Context HttpServletRequest request) throws URISyntaxException {
+		StringBuilder redirectUri = new StringBuilder(logoutUri);
+		redirectUri.append(request.getScheme());
+		redirectUri.append("://");
+		redirectUri.append(request.getServerName());
+		redirectUri.append("/#/login");
+		return Response.noContent()
+				.location(new URI(redirectUri.toString()))
+				.build();
 	}
 }
