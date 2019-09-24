ce232d15855ac409923276e0501d040c20a1fac6
AdamsDisturber <adamsdisturber@gmail.com>
2019-09-24 17:12:14 +0300

    Updated node.js to v12.

--- infrastructure-provisioning/src/general/lib/os/debian/notebook_lib.py
+++ infrastructure-provisioning/src/general/lib/os/debian/notebook_lib.py
@@ -352,7 +352,7 @@ def install_livy_dependencies_emr(os_user):
 
 def install_nodejs(os_user):
     if not exists('/home/{}/.ensure_dir/nodejs_ensured'.format(os_user)):
-        sudo('curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -')
+        sudo('curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -')
         sudo('apt-get install -y nodejs')
         sudo('touch /home/{}/.ensure_dir/nodejs_ensured'.format(os_user))
 
