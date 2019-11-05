ea9265f6232b925a6474668d209624612a5985af
Oleh Fuks <olegfuks98@gmail.com>
2019-11-05 16:00:54 +0200

    [DLAB-1233] Fixed bug with disconnecting project endpoint[Quota]

--- services/self-service/src/main/java/com/epam/dlab/backendapi/service/impl/EnvironmentServiceImpl.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/service/impl/EnvironmentServiceImpl.java
@@ -127,9 +127,11 @@ public class EnvironmentServiceImpl implements EnvironmentService {
 		checkProjectResourceConditions(project, "stop");
 		exploratoryDAO.fetchRunningExploratoryFieldsForProject(project)
 				.forEach(this::stopNotebook);
-		/*if (projectService.get(project).getStatus() == ProjectDTO.Status.ACTIVE) {
-			projectService.stop(systemUserInfoService.create("admin"), project);
-		}*/
+
+		projectService.get(project).getEndpoints().stream()
+				.filter(e -> UserInstanceStatus.RUNNING == e.getStatus())
+				.forEach(endpoint -> projectService.stop(securityService.getServiceAccountInfo("admin"),
+						endpoint.getName(), project));
 	}
 
 	@Override
