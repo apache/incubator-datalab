893b2c01311c686c95d0d3c457a95860907c8a19
Demyan Mysakovets <demian.mysakovets1@gmail.com>
2019-11-07 14:16:26 +0200

    [DLAB-1249] Fixed notebook secondary image creation

--- infrastructure-provisioning/src/general/scripts/gcp/rstudio_configure.py
+++ infrastructure-provisioning/src/general/scripts/gcp/rstudio_configure.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     else:
         notebook_config['expected_primary_image_name'] = '{}-{}-{}-primary-image'.format(
             notebook_config['service_base_name'], notebook_config['endpoint_tag'], os.environ['application'])
-        notebook_config['expected_secondary_image_name'] = '{}-{}-{}-secondary-image'.format(
+        notebook_config['expected_secondary_image_name'] = '{}-{}-{}-{}-secondary-image'.format(
             notebook_config['service_base_name'], notebook_config['endpoint_tag'], os.environ['application'])
         notebook_config['image_labels'] = {"sbn": notebook_config['service_base_name'],
                                            "endpoint_tag": notebook_config['endpoint_tag'],
