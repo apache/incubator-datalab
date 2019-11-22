a8a389500aa08949c297b4ce2128fcd24c3a2f54
Andriana Kovalyshyn <Andriana_Kovalyshyn@epam.com>
2019-11-22 16:03:56 +0200

    [DLAB-1300]: extended validation for projrct stopping/termination]

--- services/self-service/src/main/resources/webapp/src/app/administration/management/management.component.ts
+++ services/self-service/src/main/resources/webapp/src/app/administration/management/management.component.ts
@@ -95,7 +95,7 @@ export class ManagementComponent implements OnInit {
   }
 
   openManageEnvironmentDialog() {
-    this.projectService.getProjectsList().subscribe(projectsList => {
+    this.projectService.getProjectsManagingList().subscribe(projectsList => {
       this.getTotalBudgetData().subscribe(total => {
         this.dialogRef = this.dialog.open(ManageEnvironmentComponent, { data: { projectsList, total }, panelClass: 'modal-xl-s' });
         this.dialogRef.componentInstance.manageEnv.subscribe((data) => this.manageEnvironment(data));
