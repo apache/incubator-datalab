1e6386176d7b6323f46671aebdd4c34f7d3e007e
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2019-11-27 17:00:35 +0200

    added step-ca

--- infrastructure-provisioning/terraform/gcp/ssn-gke/main/modules/helm_charts/step-issuer.tf
+++ infrastructure-provisioning/terraform/gcp/ssn-gke/main/modules/helm_charts/step-issuer.tf
@@ -46,7 +46,7 @@ resource "null_resource" "step_issuer_delay" {
 data "template_file" "step_ca_issuer_values" {
   template = file("./modules/helm_charts/step-ca-issuer-chart/values.yaml")
   vars     = {
-    step_ca_url      = "https://step-certs" # "https://${kubernetes_service.step_service_lb.load_balancer_ingress.0.ip}"
+    step_ca_url      = "https://${kubernetes_service.step_service_lb.load_balancer_ingress.0.ip}"
     step_ca_bundle   = lookup(data.external.step-ca-config-values.result, "rootCa")
     namespace        = kubernetes_namespace.dlab-namespace.metadata[0].name
     step_ca_kid_name = lookup(data.external.step-ca-config-values.result, "kidName")
