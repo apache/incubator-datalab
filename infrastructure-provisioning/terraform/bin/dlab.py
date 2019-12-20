99a44e75cd9e75931ecd8880c6191f9a0ab6b729
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2019-12-20 12:21:01 +0200

    added gcp gke deploying

--- infrastructure-provisioning/terraform/bin/dlab.py
+++ infrastructure-provisioning/terraform/bin/dlab.py
@@ -1030,7 +1030,7 @@ class GCPK8sSourceBuilder(AbstractDeployBuilder):
          .add_str('--state', 'State file path', group='service')
          .add_str('--namespace', 'Name of namespace', group='k8s')
          .add_str('--credentials_file_path', 'Path to creds file', group='k8s', required=True)
-         .add_str('--project_id', 'Project ID', group='k8s', required=True)
+         .add_str('--gcp_project_id', 'Project ID', group='k8s', required=True)
          .add_str('--region', 'Region name', group='k8s', required=True)
          .add_str('--zone', 'Zone name', group='k8s', required=True)
          .add_str('--vpc_name', 'VPC name', group='k8s')
