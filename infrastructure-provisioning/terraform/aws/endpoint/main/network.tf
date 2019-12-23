471bd0396d6c20ef99bd1aa0f700253ee06661f3
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2019-12-23 15:58:25 +0200

    added gcp gke deploying

--- infrastructure-provisioning/terraform/aws/endpoint/main/network.tf
+++ infrastructure-provisioning/terraform/aws/endpoint/main/network.tf
@@ -72,7 +72,7 @@ resource "aws_subnet" "endpoint_subnet" {
 }
 
 data "aws_subnet" "data_subnet" {
-  id = var.ssn_subnet == "" ? aws_subnet.endpoint_subnet.0.id : var.ssn_subnet
+  id = var.subnet_id == "" ? aws_subnet.endpoint_subnet.0.id : var.subnet_id
 }
 
 resource "aws_route" "route" {
