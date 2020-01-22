5d85fbdb852d54739b736c8be30394c040d1dbfe
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2020-01-22 12:59:08 +0200

    [DLAB-1469]: Fixed issue with parameters

--- infrastructure-provisioning/terraform/aws/endpoint/main/outputs.tf
+++ infrastructure-provisioning/terraform/aws/endpoint/main/outputs.tf
@@ -22,3 +22,11 @@
 output "endpoint_eip_address" {
   value = aws_eip.endpoint_eip.public_ip
 }
+
+output "subnet_id" {
+  value = data.aws_subnet.data_subnet.id
+}
+
+output "vpc_id" {
+  value = data.aws_vpc.data_vpc.id
+}
