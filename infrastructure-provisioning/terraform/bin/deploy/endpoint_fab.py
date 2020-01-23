b35145f5990dc69bbf5192e7017f93406af127d9
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2020-01-23 17:09:47 +0200

    [DLAB-1472]: fixed issue with local endpoint

--- infrastructure-provisioning/terraform/bin/deploy/endpoint_fab.py
+++ infrastructure-provisioning/terraform/bin/deploy/endpoint_fab.py
@@ -514,13 +514,13 @@ def ensure_jar_endpoint():
             if args.cloud_provider == "aws":
                 conn.run('wget -P {}  --user={} --password={} '
                          'https://{}/repository/packages/aws/provisioning-service-'
-                         '2.1.jar --no-check-certificate'
+                         '2.2.jar --no-check-certificate'
                          .format(web_path, args.repository_user,
                                  args.repository_pass, args.repository_address))
             elif args.cloud_provider == "gcp":
                 conn.run('wget -P {}  --user={} --password={} '
                          'https://{}/repository/packages/gcp/provisioning-service-'
-                         '2.1.jar --no-check-certificate'
+                         '2.2.jar --no-check-certificate'
                          .format(web_path, args.repository_user,
                                  args.repository_pass, args.repository_address))
             conn.run('mv {0}/*.jar {0}/provisioning-service.jar'
