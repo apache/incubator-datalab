c09cad9c41a22bbae8cb6fb81516af3847b5f16d
Pavel Papou <pavel_papou@epam.com>
2020-02-03 19:16:08 -0500

    [DLAB-1447] Remove superflous import
     - the RegExp package is removed

--- services/self-service/src/main/java/com/epam/dlab/backendapi/service/impl/EndpointServiceImpl.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/service/impl/EndpointServiceImpl.java
@@ -27,7 +27,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Slf4j
