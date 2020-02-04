07e713c56447d283572674939a7cea41daa50783
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2020-02-04 11:50:00 +0200

    [DLAB-1467]: Added terraform scripts for deploying DLab endpoint

--- infrastructure-provisioning/terraform/azure/endpoint/main/network.tf
+++ infrastructure-provisioning/terraform/azure/endpoint/main/network.tf
@@ -84,7 +84,7 @@ resource "azurerm_network_interface" "endpoint-nif" {
   ip_configuration {
     name                          = "configuration"
     subnet_id                     = data.azurerm_subnet.data-endpoint-subnet.id
-    private_ip_address_allocation = "Static"
+    private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.endpoint-static-ip.id
     private_ip_address_version    = "IPv4"
   }
