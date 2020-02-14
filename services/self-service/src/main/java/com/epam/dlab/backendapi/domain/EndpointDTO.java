134fe6e0b036d4658ba77c8990069093231ab59b
Pavel Papou <ppapou27@gmail.com>
2020-02-14 09:44:54 -0500

    [Dlab-1422] The endpoint URL verification (#588)
    [Dlab-1422] The endpoint URL verification

--- services/self-service/src/main/java/com/epam/dlab/backendapi/domain/EndpointDTO.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/domain/EndpointDTO.java
@@ -23,14 +23,20 @@ import com.epam.dlab.cloud.CloudProvider;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.URL;
 
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EndpointDTO {
 
+	private static final String URL_REGEXP_VALIDATION = "^(http(s)?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+	@NotEmpty
+	@NotBlank
 	private final String name;
-
+	@URL(regexp = URL_REGEXP_VALIDATION, message = "endpoint field is in improper format!")
 	private final String url;
 	private final String account;
 	@JsonProperty("endpoint_tag")
