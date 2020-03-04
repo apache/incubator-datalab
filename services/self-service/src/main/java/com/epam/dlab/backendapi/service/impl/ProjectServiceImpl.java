b4a06f958b97e3c3e5d48335098354937caaf724
Pavel Papou <pavel_papou@epam.com>
2020-03-03 21:57:42 -0500

    [DLAb-1520] minor method adkustment
     - filter expression of the getProgectsForMapping() method has been
     adjusted

--- services/self-service/src/main/java/com/epam/dlab/backendapi/service/impl/ProjectServiceImpl.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/service/impl/ProjectServiceImpl.java
@@ -74,7 +74,7 @@ public class ProjectServiceImpl implements ProjectService {
 	public List<ProjectManagingDTO> getProjectsForManaging() {
 		return projectDAO.getProjects().stream().map(p -> new ProjectManagingDTO(
 				p.getName(), p.getBudget(), isCanBeStopped(p), isCanBeTerminated(p)))
-				.filter(projectManagingDTO -> projectManagingDTO.isCanBeTerminated())
+				.filter(ProjectManagingDTO::isCanBeTerminated)
 				.collect(Collectors.toList());
 	}
 
