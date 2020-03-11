3759b3a2323ed2b2deb3e5d996a6e166f03f4db8
leonidfrolov <frolovleon@gmail.com>
2020-03-11 18:06:38 +0200

    [odahu-integration]: changed access links for notebooks in case domain name is specified

--- infrastructure-provisioning/src/general/scripts/gcp/jupyter_configure.py
+++ infrastructure-provisioning/src/general/scripts/gcp/jupyter_configure.py
@@ -85,7 +85,10 @@ if __name__ == "__main__":
         edge_instance_name = '{0}-{1}-{2}-edge'.format(notebook_config['service_base_name'],
                                                        notebook_config['project_name'],
                                                        notebook_config['endpoint_name'])
-        edge_instance_hostname = GCPMeta.get_instance_public_ip_by_name(edge_instance_name)
+        if os.environ['conf_domain_name_enabled'] and 'conf_domain_name' in os.environ:
+            edge_instance_hostname = '{}.{}'.format(notebook_config['project_name'], os.environ['conf_domain_name'])
+        else:
+            edge_instance_hostname = GCPMeta.get_instance_public_ip_by_name(edge_instance_name)
         edge_instance_private_ip = GCPMeta.get_private_ip_address(edge_instance_name)
         notebook_config['ssh_key_path'] = '{0}{1}.pem'.format(os.environ['conf_key_dir'], os.environ['conf_key_name'])
         notebook_config['dlab_ssh_user'] = os.environ['conf_os_user']
