fb3dd9d352d2c99a5a8faaa7ec8a11ef25e10abc
Mykola_Bodnar1 <bodnarmykola@gmail.com>
2020-03-12 11:57:45 +0200

    [DLAB-1565]: Convey billing for remote endpoints - Mongo on endpoint installation fixed

--- infrastructure-provisioning/terraform/bin/deploy/endpoint_fab.py
+++ infrastructure-provisioning/terraform/bin/deploy/endpoint_fab.py
@@ -253,8 +253,8 @@ def ensure_mongo_endpoint():
         print('[INSTALLING MONGO DATABASE]')
         if not exists(conn, '/home/{}/.ensure_dir/mongo_ensured'.format(args.os_user)):
             conn.sudo('apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927')
-            conn.sudo('bash -c ver=`lsb_release -cs`; '
-                      'echo "deb http://repo.mongodb.org/apt/ubuntu $ver/mongodb-org/3.2 multiverse" | '
+            conn.sudo('bash -c '
+                      'echo "deb http://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/3.2 multiverse" | '
                       'sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list; '
                       'sudo apt-get update')
             conn.sudo('apt-get install mongodb-org -y --allow-unauthenticated install')
