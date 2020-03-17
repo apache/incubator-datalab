74f89664d023cae8963cab03bf4c73a893b0cbfd
Demyan Mysakovets <demian.mysakovets1@gmail.com>
2020-03-17 19:16:33 +0200

    [odahu-integration] Bugfix

--- infrastructure-provisioning/src/ssn/scripts/configure_docker.py
+++ infrastructure-provisioning/src/ssn/scripts/configure_docker.py
@@ -89,7 +89,9 @@ def login_in_gcr(gcr_creds, odahu_image, dlab_path):
             local('scp -i {} /tmp/dlab-gcr-ro-sa {}:/tmp/dlab-gcr-ro-sa'.format(args.keyfile, env.host_string))
             sudo('cat /tmp/dlab-gcr-ro-sa | base64 --decode > /tmp/dlab-gcr-ro-sa.json')
             sudo('cat /tmp/dlab-gcr-ro-sa.json | docker login -u _json_key --password-stdin https://gcr.io')
-            sudo('sed -i "s|ODAHU_IMAGE|{}|" {}general/files/gcp/odahu_Dockerfile'.format(odahu_image, dlab_path))
+            sudo('sed -i "s|ODAHU_IMAGE|{}|" '
+                 '{}sources/infrastructure-provisioning/src/general/files/gcp/odahu_Dockerfile'.format(odahu_image,
+                                                                                                       dlab_path))
         except Exception as err:
             traceback.print_exc()
             print('Failed to prepare odahu image: ', str(err))
