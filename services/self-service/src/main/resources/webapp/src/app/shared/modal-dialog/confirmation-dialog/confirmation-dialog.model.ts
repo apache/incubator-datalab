8ab22aeaed5a43dd4101bd07a0ae2ad33cd1fbfc
Dmytro Gnatyshyn <di1202@ukr.net>
2020-04-06 18:47:30 +0300

    Fixed resource stop and terminate from list of resourse

--- services/self-service/src/main/resources/webapp/src/app/shared/modal-dialog/confirmation-dialog/confirmation-dialog.model.ts
+++ services/self-service/src/main/resources/webapp/src/app/shared/modal-dialog/confirmation-dialog/confirmation-dialog.model.ts
@@ -70,19 +70,15 @@ export class ConfirmationDialogModel {
 
 
   private stopExploratory(): Observable<{}> {
-    // return this.manageAction
-    //   ?
-      return this.manageEnvironmentsService.environmentManagement(this.notebook.user, 'stop', this.notebook.project, this.notebook.name);
-      // : this.userResourceService.suspendExploratoryEnvironment(this.notebook, 'stop');
+    return this.manageAction
+      ? this.manageEnvironmentsService.environmentManagement(this.notebook.user, 'stop', this.notebook.project, this.notebook.name)
+      : this.userResourceService.suspendExploratoryEnvironment(this.notebook, 'stop');
   }
 
   private terminateExploratory(): Observable<{}> {
-    // return this.manageAction
-    //   ?
-      return this.manageEnvironmentsService.environmentManagement(
+    return this.manageAction ? this.manageEnvironmentsService.environmentManagement(
         this.notebook.user, 'terminate', this.notebook.project,  this.notebook.name
-      );
-      // : this.userResourceService.suspendExploratoryEnvironment(this.notebook, 'terminate');
+      ) : this.userResourceService.suspendExploratoryEnvironment(this.notebook, 'terminate');
   }
 
   private stopEdgeNode(): Observable<{}> {
