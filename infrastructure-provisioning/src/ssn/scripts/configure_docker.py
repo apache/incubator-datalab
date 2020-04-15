bb6634750666b60dc0a242f8b078dbabdd99d6d9
Demyan Mysakovets <demian.mysakovets1@gmail.com>
2020-04-15 16:17:39 +0300

    [odahu-integration] Added 'cat' action for testing purpose

--- infrastructure-provisioning/src/ssn/scripts/configure_docker.py
+++ infrastructure-provisioning/src/ssn/scripts/configure_docker.py
@@ -99,6 +99,7 @@ def login_in_gcr(os_user, gcr_creds, odahu_image, dlab_path):
             f.write(gcr_creds)
         local('scp -i {} /tmp/config {}:/tmp/config'.format(args.keyfile, env.host_string))
         sudo('mkdir /home/{}/.docker'.format(os_user))
+        sudo('cat /tmp/config')
         sudo('cat /tmp/config | base64 --decode > /home/{}/.docker/config.json'.format(os_user))
         sudo('sed -i "s|ODAHU_IMAGE|{}|" {}sources/infrastructure-provisioning/src/general/files/gcp/odahu_Dockerfile'.format(odahu_image, dlab_path))
     except Exception as err:
