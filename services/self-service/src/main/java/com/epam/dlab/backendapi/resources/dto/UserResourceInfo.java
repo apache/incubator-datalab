69c7b48c3b54af578a7ebc6c9062ca55f37a553b
Oleh Fuks <olegfuks98@gmail.com>
2020-05-25 12:21:51 +0300

    [DLAB-1744] Conveyed notebook links of other users to administrator

--- services/self-service/src/main/java/com/epam/dlab/backendapi/resources/dto/UserResourceInfo.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/resources/dto/UserResourceInfo.java
@@ -18,88 +18,38 @@
  */
 package com.epam.dlab.backendapi.resources.dto;
 
+import com.epam.dlab.dto.ResourceURL;
 import com.epam.dlab.dto.computational.UserComputationalResource;
 import com.epam.dlab.model.ResourceEnum;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Data;
 
-import java.util.Collections;
 import java.util.List;
 
 @Data
+@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserResourceInfo {
-
-	@JsonProperty("resource_type")
-	private ResourceEnum resourceType;
-
-	@JsonProperty("resource_name")
-	private String resourceName;
-
-	@JsonProperty("shape")
-	private String resourceShape;
-
-	@JsonProperty("status")
-	private String resourceStatus;
-
-	@JsonProperty("computational_resources")
-	private List<UserComputationalResource> computationalResources = Collections.emptyList();
-
 	@JsonProperty
 	private String user;
 	@JsonProperty
 	private String project;
-
+	@JsonProperty("resource_type")
+	private ResourceEnum resourceType;
+	@JsonProperty("resource_name")
+	private String resourceName;
+	@JsonProperty("shape")
+	private String resourceShape;
+	@JsonProperty("status")
+	private String resourceStatus;
+	@JsonProperty("computational_resources")
+	private List<UserComputationalResource> computationalResources;
 	@JsonProperty("public_ip")
 	private String ip;
-
 	@JsonProperty("cloud_provider")
 	private String cloudProvider;
-
-
-	public UserResourceInfo withResourceType(ResourceEnum resourceType) {
-		setResourceType(resourceType);
-		return this;
-	}
-
-	public UserResourceInfo withResourceName(String resourceName) {
-		setResourceName(resourceName);
-		return this;
-	}
-
-	public UserResourceInfo withResourceShape(String resourceShape) {
-		setResourceShape(resourceShape);
-		return this;
-	}
-
-	public UserResourceInfo withResourceStatus(String resourceStatus) {
-		setResourceStatus(resourceStatus);
-		return this;
-	}
-
-	public UserResourceInfo withCompResources(List<UserComputationalResource> compResources) {
-		setComputationalResources(compResources);
-		return this;
-	}
-
-	public UserResourceInfo withUser(String user) {
-		setUser(user);
-		return this;
-	}
-
-	public UserResourceInfo withIp(String ip) {
-		setIp(ip);
-		return this;
-	}
-
-	public UserResourceInfo withProject(String project) {
-		setProject(project);
-		return this;
-	}
-
-	public UserResourceInfo withCloudProvider(String cloudProvider) {
-		setCloudProvider(cloudProvider);
-		return this;
-	}
+	@JsonProperty("exploratory_urls")
+	private List<ResourceURL> exploratoryUrls;
 }
