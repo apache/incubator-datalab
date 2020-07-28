c5862487a87bc8ac58b9fa6750762f5577904430
Oleh Fuks <olegfuks98@gmail.com>
2020-07-28 18:16:26 +0300

    Created test for AccessKeyServiceImpl

---
+++ services/self-service/src/test/java/com/epam/dlab/backendapi/service/impl/AccessKeyServiceImplTest.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.epam.dlab.backendapi.service.impl;
+
+import com.epam.dlab.auth.UserInfo;
+import com.epam.dlab.backendapi.conf.SelfServiceApplicationConfiguration;
+import com.epam.dlab.backendapi.resources.TestBase;
+import com.epam.dlab.backendapi.resources.dto.KeysDTO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AccessKeyServiceImplTest extends TestBase {
+
+	@Mock
+	private SelfServiceApplicationConfiguration conf;
+
+	@InjectMocks
+	private AccessKeyServiceImpl accessKeyService;
+
+
+	@Test
+	public void generateKeys() {
+		UserInfo userInfo = getUserInfo();
+		when(conf.getPrivateKeySize()).thenReturn(2048);
+
+		KeysDTO keysDTO = accessKeyService.generateKeys(userInfo);
+
+		assertEquals(USER.toLowerCase(), keysDTO.getUsername());
+		assertNotNull(keysDTO.getPublicKey());
+		assertNotNull(keysDTO.getPrivateKey());
+	}
+}
