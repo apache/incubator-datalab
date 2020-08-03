1b367b7c1c6247a6f2280e6b583a1dcc21df38f7
Dmytro_Gnatyshyn <di1202@ukr.net>
2020-08-03 11:39:49 +0300

    [DLAB-1971]: UI changes for all pages (part2)

--- services/self-service/src/main/resources/webapp/src/app/reports/reporting/reporting-grid/reporting-grid.component.ts
+++ services/self-service/src/main/resources/webapp/src/app/reports/reporting/reporting-grid/reporting-grid.component.ts
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import {Component, OnInit, Output, EventEmitter, ViewChild, Input} from '@angular/core';
+import {Component, OnInit, Output, EventEmitter, ViewChild, Input, HostListener, AfterViewInit, ChangeDetectorRef} from '@angular/core';
 import { ReportingConfigModel } from '../../../../dictionary/global.dictionary';
 
 @Component({
@@ -27,7 +27,7 @@ import { ReportingConfigModel } from '../../../../dictionary/global.dictionary';
     '../../../resources/resources-grid/resources-grid.component.scss'],
 
 })
-export class ReportingGridComponent implements OnInit {
+export class ReportingGridComponent implements OnInit, AfterViewInit {
 
   filterConfiguration: ReportingConfigModel;
   // filteredReportData: ReportingConfigModel = new ReportingConfigModel([], [], [], [], [], '', '', '', []);
@@ -38,15 +38,42 @@ export class ReportingGridComponent implements OnInit {
   active: object = {};
 
   @ViewChild('nameFilter', { static: false }) filter;
+  @ViewChild('tableWrapper', { static: false }) tableWrapper;
+  @ViewChild('table', { static: false }) table;
 
   @Output() filterReport: EventEmitter<{}> = new EventEmitter();
   @Output() resetRangePicker: EventEmitter<boolean> = new EventEmitter();
   @Input() filteredReportData: ReportingConfigModel;
+  private isScrollButtonsVisible: boolean;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.isScrollButtonsVisible = this.tableWrapper.nativeElement.offsetWidth - this.table._elementRef.nativeElement.offsetWidth < 0;
+    this.isMaxRight = this.tableWrapper.nativeElement.offsetWidth + this.tableWrapper.nativeElement.scrollLeft + 2 <= this.table._elementRef.nativeElement.offsetWidth;
+  }
+  @HostListener('scroll', ['$event'])
+  scrollTable($event: Event) {
+    this.isMaxRight = this.tableWrapper.nativeElement.offsetWidth + this.tableWrapper.nativeElement.scrollLeft + 2 <= this.table._elementRef.nativeElement.offsetWidth;
+  }
+
+
+
   displayedColumns: string[] = ['name', 'user', 'project', 'type', 'status', 'shape', 'service', 'empty', 'charge'];
   displayedFilterColumns: string[] = ['name-filter', 'user-filter', 'project-filter', 'type-filter', 'status-filter', 'shape-filter',  'service-filter', 'empty-filter', 'actions'];
   filtered: any;
+  isMaxRight: boolean;
+
+  constructor(private changeDetector: ChangeDetectorRef) {
+  }
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    window.setTimeout(() => {
+      this.isScrollButtonsVisible = this.tableWrapper.nativeElement.offsetWidth - this.table._elementRef.nativeElement.offsetWidth < 0;
+      this.isMaxRight = this.tableWrapper.nativeElement.offsetWidth + this.tableWrapper.nativeElement.scrollLeft !== this.table._elementRef.nativeElement.offsetWidth;
+    }, 500);
+  }
 
   onUpdate($event): void {
     this.filteredReportData[$event.type] = $event.model;
@@ -116,4 +143,13 @@ export class ReportingGridComponent implements OnInit {
   shapeSplit(shape) {
     return shape.split(/(?=Slave)/g);
   }
+
+  public sctollTo(direction: string) {
+    if (direction === 'left') {
+      this.tableWrapper.nativeElement.scrollLeft = 0;
+    } else {
+      this.tableWrapper.nativeElement.scrollLeft = this.tableWrapper.nativeElement.offsetWidth;
+    }
+  }
+
 }
