54d76b1f3923d66462280a8d29abc2a3ce956bdf
Pavel Papou <ppapou27@gmail.com>
2020-09-01 07:13:48 -0400

    Constant fix

--- services/self-service/src/test/java/com/epam/dlab/backendapi/resources/ProjectResourceTest.java
+++ services/self-service/src/test/java/com/epam/dlab/backendapi/resources/ProjectResourceTest.java
@@ -58,7 +58,7 @@ import static org.mockito.Mockito.when;
 
 public class ProjectResourceTest extends TestBase {
 
-    private final static String PROJECT_NAME = "DLAB";
+    private static final String PROJECT_NAME = "DLAB";
 
     private final ProjectService projectService = mock(ProjectService.class);
     private final AccessKeyService keyService = mock(AccessKeyService.class);
