37c484be22a0d93e915e4904613c6118c7a1d878
Dmytro Gnatyshyn <42860905+DG1202@users.noreply.github.com>
2020-10-07 10:30:54 +0300

    [DATALAB-2088]: Fixed UI issues connected with name replacement (#938)
    [DATALAB-2088]: Fixed UI issues connected with name replacement

--- services/self-service/src/main/resources/webapp/src/dictionary/global.dictionary.ts
+++ services/self-service/src/main/resources/webapp/src/dictionary/global.dictionary.ts
@@ -43,7 +43,7 @@ export class ReportingConfigModel {
     public shapes: Array<string>,
     public date_start: string,
     public date_end: string,
-    public datalab_id: string,
+    public datalabId: string,
     public projects: Array<string>
   ) { }
 
@@ -55,7 +55,7 @@ export class ReportingConfigModel {
     this.shapes = [];
     this.date_start = '';
     this.date_end = '';
-    this.datalab_id = '';
+    this.datalabId = '';
     this.projects = [];
   }
 }
