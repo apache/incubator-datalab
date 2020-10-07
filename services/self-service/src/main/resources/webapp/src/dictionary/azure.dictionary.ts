f26258acbae2179efab5d868381d967a3f513c9a
Dmytro_Gnatyshyn <di1202@ukr.net>
2020-10-07 10:20:22 +0300

    [DATALAB-2088]: Fixed UI issues connected with name replacement

--- services/self-service/src/main/resources/webapp/src/dictionary/azure.dictionary.ts
+++ services/self-service/src/main/resources/webapp/src/dictionary/azure.dictionary.ts
@@ -44,7 +44,7 @@ export const NAMING_CONVENTION_AZURE = {
         'type': '',
         'resourceType': 'resource_type',
         'instance_size': 'size',
-        'datalab_id': 'datalab_id'
+        'datalabId': 'datalabId'
     },
     'service': 'Category',
     'type': '',
