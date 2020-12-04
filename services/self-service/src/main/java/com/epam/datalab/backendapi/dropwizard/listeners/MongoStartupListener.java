a5bb423008d75e84181c1a043ea7dcc1a77bfb21
Kinash Yurii <ura.kinash@gmail.com>
2020-12-04 20:05:09 +0200

    [DATALAB-2175] -- added roles for local endpoints (#1002)
    [DATALAB-2175] -- added roles for local endpoints

--- services/self-service/src/main/java/com/epam/datalab/backendapi/dropwizard/listeners/MongoStartupListener.java
+++ services/self-service/src/main/java/com/epam/datalab/backendapi/dropwizard/listeners/MongoStartupListener.java
@@ -23,7 +23,9 @@ import com.epam.datalab.backendapi.conf.SelfServiceApplicationConfiguration;
 import com.epam.datalab.backendapi.dao.EndpointDAO;
 import com.epam.datalab.backendapi.dao.SettingsDAO;
 import com.epam.datalab.backendapi.dao.UserRoleDAO;
+import com.epam.datalab.backendapi.domain.EndpointDTO;
 import com.epam.datalab.backendapi.resources.dto.UserRoleDTO;
+import com.epam.datalab.cloud.CloudProvider;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
@@ -38,7 +40,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
+import static java.lang.String.format;
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toCollection;
@@ -46,11 +50,12 @@ import static java.util.stream.Collectors.toCollection;
 @Slf4j
 public class MongoStartupListener implements ServerLifecycleListener {
 
-    private static final String PATH_TO_GENERAL_ROLES = "/mongo/general/mongo_roles.json";
+    private static final String PATH_TO_ROLES = "/mongo/%s/mongo_roles.json";
     private static final ObjectMapper MAPPER = new ObjectMapper();
     private final UserRoleDAO userRoleDao;
     private final SelfServiceApplicationConfiguration configuration;
     private final SettingsDAO settingsDAO;
+    private final EndpointDAO endpointDAO;
 
     @Inject
     public MongoStartupListener(UserRoleDAO userRoleDao, SelfServiceApplicationConfiguration configuration,
@@ -58,6 +63,7 @@ public class MongoStartupListener implements ServerLifecycleListener {
         this.userRoleDao = userRoleDao;
         this.configuration = configuration;
         this.settingsDAO = settingsDAO;
+        this.endpointDAO = endpointDAO;
     }
 
     @Override
@@ -65,23 +71,35 @@ public class MongoStartupListener implements ServerLifecycleListener {
         settingsDAO.setServiceBaseName(configuration.getServiceBaseName());
         settingsDAO.setConfOsFamily(configuration.getOs());
         settingsDAO.setSsnInstanceSize(configuration.getSsnInstanceSize());
+        List<EndpointDTO> endpointDTOs = endpointDAO.getEndpoints();
         if (userRoleDao.findAll().isEmpty()) {
-            log.debug("Populating DataLab roles into database");
-            userRoleDao.insert(getRoles());
+            log.info("Populating DataLab default roles into database");
+            List<UserRoleDTO> cloudRoles = getRoles(CloudProvider.GENERAL);
+            List<CloudProvider> connectedCloudProviders = getConnectedProviders(endpointDTOs);
+            log.info("Check for connected endpoints:\n connected endpoints: {} \n connected clouds: {}",
+                    endpointDTOs.size(), connectedCloudProviders);
+            connectedCloudProviders.forEach(provider -> cloudRoles.addAll(getRoles(provider)));
+            userRoleDao.insert(cloudRoles);
         } else {
             log.info("Roles already populated. Do nothing ...");
         }
     }
 
-    private List<UserRoleDTO> getRoles() {
-        Set<UserRoleDTO> userRoles = new HashSet<>(getUserRoleFromFile());
+    private List<CloudProvider> getConnectedProviders(List<EndpointDTO> endpointDTOs) {
+        return endpointDTOs.stream()
+                .map(EndpointDTO::getCloudProvider)
+                .collect(Collectors.toList());
+    }
+
+    private List<UserRoleDTO> getRoles(CloudProvider cloudProvider) {
+        Set<UserRoleDTO> userRoles = new HashSet<>(getUserRoleFromFile(cloudProvider));
         return userRoles.stream()
                 .collect(collectingAndThen(toCollection(() -> new TreeSet<>(comparing(UserRoleDTO::getId))),
                         ArrayList::new));
     }
 
-    private List<UserRoleDTO> getUserRoleFromFile() {
-        try (InputStream is = getClass().getResourceAsStream(PATH_TO_GENERAL_ROLES)) {
+    private List<UserRoleDTO> getUserRoleFromFile(CloudProvider cloudProvider) {
+        try (InputStream is = getClass().getResourceAsStream(format(PATH_TO_ROLES, cloudProvider.getName()))) {
             return MAPPER.readValue(is, new TypeReference<List<UserRoleDTO>>() {
             });
         } catch (IOException e) {
