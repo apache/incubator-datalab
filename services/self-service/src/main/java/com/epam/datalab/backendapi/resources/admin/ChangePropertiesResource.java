1ff289ea7874254ea8e44e9e651a63241562dad5
KinashYurii <ura.kinash@gmail.com>
2020-12-14 18:13:00 +0200

    [DATALAB-2174] -- added admin resource with dynamic change for properties(self/provisioning-service)

--- services/self-service/src/main/java/com/epam/datalab/backendapi/resources/admin/ChangePropertiesResource.java
+++ services/self-service/src/main/java/com/epam/datalab/backendapi/resources/admin/ChangePropertiesResource.java
@@ -1,64 +1,89 @@
 package com.epam.datalab.backendapi.resources.admin;
 
 import com.epam.datalab.auth.UserInfo;
-import com.epam.datalab.backendapi.resources.dto.BillingFilter;
 import com.epam.datalab.backendapi.roles.UserRoles;
+import com.epam.datalab.backendapi.service.impl.DynamicChangeProperties;
 import io.dropwizard.auth.Auth;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.inject.Inject;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("admin")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ChangePropertiesResource {
 
+    private final DynamicChangeProperties dynamic;
+
+    @Inject
+    public ChangePropertiesResource(DynamicChangeProperties dynamic) {
+        this.dynamic = dynamic;
+    }
+
     @GET
     @Path("/self-service")
-    @Produces(MediaType.APPLICATION_JSON)
     public Response getSelfServiceProperties(@Auth UserInfo userInfo) {
-        if (UserRoles.isAdmin(userInfo))
-            return Response.ok(DinamycChangeProperties.getSelfServiceProperties());
-        else return Response.status(Response.Status.FORBIDDEN).build();
+        if (UserRoles.isAdmin(userInfo)) {
+            return Response
+                    .ok(dynamic.getSelfServiceProperties())
+                    .build();
+        }
+        return Response
+                .status(Response.Status.FORBIDDEN)
+                .build();
     }
 
     @GET
     @Path("/provisioning-service")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getProvisioningServiceProperties(@Auth UserInfo userInfo, @Valid @NotNull BillingFilter filter) {
-        if (UserRoles.isAdmin(userInfo))
-            return Response.ok(DinamycChangeProperties.getProvisioningServiceProperties());
-        else return Response.status(Response.Status.FORBIDDEN).build();
+    public Response getProvisioningServiceProperties(@Auth UserInfo userInfo) {
+        if (UserRoles.isAdmin(userInfo)) {
+            return Response
+                    .ok(dynamic.getProvisioningServiceProperties())
+                    .build();
+        } else {
+            return Response
+                    .status(Response.Status.FORBIDDEN)
+                    .build();
+        }
     }
 
     @POST
     @Path("/self-service")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response setSelfServiceProperties(@Auth UserInfo userInfo) {
-        if (UserRoles.isAdmin(userInfo))
-            return Response.ok(DinamycChangeProperties.setSelfServiceProperties());
-        else return Response.status(Response.Status.FORBIDDEN).build();
+    public Response overwriteSelfServiceProperties(@Auth UserInfo userInfo, String ymlString) {
+        if (UserRoles.isAdmin(userInfo)) {
+            dynamic.overwriteSelfServiceProperties(ymlString);
+            return Response.ok().build();
+        }
+        return Response
+                .status(Response.Status.FORBIDDEN)
+                .build();
     }
 
     @POST
     @Path("/provisioning-service")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response setProvisioningServiceProperties(@Auth UserInfo userInfo) {
-        if (UserRoles.isAdmin(userInfo))
-            return Response.ok(DinamycChangeProperties.setProvisioningServiceProperties());
-        else return Response.status(Response.Status.FORBIDDEN).build();
+    public Response overwriteProvisioningServiceProperties(@Auth UserInfo userInfo, String ymlString) {
+        if (UserRoles.isAdmin(userInfo)) {
+            dynamic.overwriteProvisioningServiceProperties(ymlString);
+            return Response.ok().build();
+        } else {
+            return Response
+                    .status(Response.Status.FORBIDDEN)
+                    .build();
+        }
     }
 
-    @POST
-    @Path("/restart")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response restart(@Auth UserInfo userInfo, boolean restartSelfService, boolean restartProvisioning) {
-        if (UserRoles.isAdmin(userInfo))
-            return Response.ok(DinamycChangeProperties.restart());
-        else return Response.status(Response.Status.FORBIDDEN).build();
-    }
+//    @POST
+//    @Path("/restart")
+//    public Response restart(@Auth UserInfo userInfo, boolean restartSelfService, boolean restartProvisioning) {
+//        if (UserRoles.isAdmin(userInfo)) {
+//            dynamic.restart(restartSelfService,restartProvisioning);
+//            return Response.ok().build();
+//        } else {
+//            return Response
+//                    .status(Response.Status.FORBIDDEN)
+//                    .build();
+//        }
+//    }
 }
---
+++ services/self-service/src/main/java/com/epam/datalab/backendapi/service/impl/DynamicChangeProperties.java
@@ -0,0 +1,166 @@
+package com.epam.datalab.backendapi.service.impl;
+
+import com.epam.datalab.exceptions.DynamicChangePropertiesException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Slf4j
+public class DynamicChangeProperties {
+
+    private static final String SELF_SERVICE = "self-service.yml";
+    private static final String SELF_SERVICE_PROP_PATH = "services/billing-azure/billing.yml";
+    private static final String PROVISIONING_SERVICE = "provisioning.yml";
+    private static final String PROVISIONING_SERVICE_PROP_PATH = "services/provisioning-service/provisioning.yml";
+    private static final String SECRET_REGEX = "(.*)[sS]ecret(.*): (.*)";
+    private static final String SECRET_REPLACEMENT_FORMAT = " ***********";
+    private static final String SH_COMMAND = "sudo supervisorctl restart ";
+
+    private static final String LICENCE =
+            "# *****************************************************************************\n" +
+                    "#\n" +
+                    "#  Licensed to the Apache Software Foundation (ASF) under one\n" +
+                    "#  or more contributor license agreements.  See the NOTICE file\n" +
+                    "#  distributed with this work for additional information\n" +
+                    "#  regarding copyright ownership.  The ASF licenses this file\n" +
+                    "#  to you under the Apache License, Version 2.0 (the\n" +
+                    "#  \"License\"); you may not use this file except in compliance\n" +
+                    "#  with the License.  You may obtain a copy of the License at\n" +
+                    "#\n" +
+                    "#  http://www.apache.org/licenses/LICENSE-2.0\n" +
+                    "#\n" +
+                    "#  Unless required by applicable law or agreed to in writing,\n" +
+                    "#  software distributed under the License is distributed on an\n" +
+                    "#  \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY\n" +
+                    "#  KIND, either express or implied.  See the License for the\n" +
+                    "#  specific language governing permissions and limitations\n" +
+                    "#  under the License.\n" +
+                    "#\n" +
+                    "# ******************************************************************************";
+
+    private static final int DEFAULT_VALUE_PLACE = 1;
+    private static final int DEFAULT_NAME_PLACE = 0;
+
+    public String getSelfServiceProperties() {
+        return readFileAsString(SELF_SERVICE_PROP_PATH, SELF_SERVICE);
+    }
+
+    public String getProvisioningServiceProperties() {
+        return readFileAsString(PROVISIONING_SERVICE_PROP_PATH, PROVISIONING_SERVICE);
+    }
+
+    public void overwriteSelfServiceProperties(String newPropFile) {
+        writeFileFromString(newPropFile, SELF_SERVICE, SELF_SERVICE_PROP_PATH);
+    }
+
+    public void overwriteProvisioningServiceProperties(String newPropFile) {
+        writeFileFromString(newPropFile, PROVISIONING_SERVICE, PROVISIONING_SERVICE_PROP_PATH);
+    }
+
+    public void restart(boolean restartSelfService, boolean restartProvisioning) {
+        StringBuilder stringBuilder = new StringBuilder(SH_COMMAND);
+        if (restartSelfService) {
+            stringBuilder.append(" self-service");
+        }
+        if (restartProvisioning) {
+            stringBuilder.append(" provisioning");
+        }
+        try {
+            Process process = Runtime.getRuntime()
+                    .exec(String.format("sh %s", stringBuilder.toString()));
+            //        String homeDirectory = System.getProperty("user.home");
+//            StreamGobbler streamGobbler =
+//                    new StreamGobbler(process.getInputStream(), System.out::println);
+//            Executors.newSingleThreadExecutor().submit(streamGobbler);
+//            int exitCode = process.waitFor();
+//            assert exitCode == 0;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // run ssh with restart supervisorctl
+    }
+
+    private String readFileAsString(String selfServicePropPath, String serviceName) {
+        try {
+            log.trace("Trying to read self-service.yml, file from path {} :", selfServicePropPath);
+            String currentConf = FileUtils.readFileToString(new File(selfServicePropPath), Charset.defaultCharset());
+            return hideSecretsAndRemoveLicence(currentConf);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new DynamicChangePropertiesException(String.format("Failed while read file %s", serviceName));
+        }
+    }
+
+    private String hideSecretsAndRemoveLicence(String currentConf) {
+        Matcher m = Pattern.compile(SECRET_REGEX).matcher(currentConf);
+        List<String> secrets = new ArrayList<>();
+        String confWithReplacedSecretConf = removeLicence(currentConf);
+        while (m.find()) {
+            secrets.add(m.group().split(":")[DEFAULT_VALUE_PLACE]);
+        }
+        for (String secret : secrets) {
+            confWithReplacedSecretConf = confWithReplacedSecretConf.replace(secret, SECRET_REPLACEMENT_FORMAT);
+        }
+        return confWithReplacedSecretConf;
+    }
+
+    private String removeLicence(String conf) {
+        return conf.replace(LICENCE, "");
+    }
+
+    private void writeFileFromString(String newPropFile, String serviceName, String servicePath) {
+        try {
+            String oldFile = FileUtils.readFileToString(new File(servicePath), Charset.defaultCharset());
+            BufferedWriter writer = new BufferedWriter(new FileWriter(servicePath));
+            log.trace("Trying to overwrite {}, file for path {} :", serviceName, servicePath);
+            writer.write(LICENCE);
+            writer.write(checkAndReplaceSecretIfEmpty(newPropFile, oldFile));
+            log.info("{} overwritten successfully", serviceName);
+            writer.close();
+        } catch (IOException e) {
+            log.error("Failed during overwriting {}", serviceName);
+            throw new DynamicChangePropertiesException(String.format("Failed during overwriting %s", serviceName));
+        }
+    }
+
+    private String checkAndReplaceSecretIfEmpty(String newPropFile, String oldProf) {
+        Map<String, String> emptySecrets = findEmptySecret(newPropFile);
+        return emptySecrets.isEmpty() ? newPropFile : replaceEmptySecret(newPropFile, oldProf, emptySecrets);
+
+    }
+
+    private String replaceEmptySecret(String newPropFile, String oldProf, Map<String, String> emptySecrets) {
+        String fileWithReplacedEmptySecrets = newPropFile;
+        Matcher oldProfMatcher = Pattern.compile(SECRET_REGEX).matcher(oldProf);
+        while (oldProfMatcher.find()) {
+            String[] s = oldProfMatcher.group().split(":");
+            if (emptySecrets.containsKey(s[DEFAULT_NAME_PLACE])) {
+                fileWithReplacedEmptySecrets = fileWithReplacedEmptySecrets.replace(emptySecrets.get(s[DEFAULT_NAME_PLACE]), oldProfMatcher.group());
+            }
+        }
+        return fileWithReplacedEmptySecrets;
+    }
+
+    private Map<String, String> findEmptySecret(String newPropFile) {
+        Matcher newPropFileMatcher = Pattern.compile(SECRET_REGEX).matcher(newPropFile);
+        Map<String, String> emptySecrets = new HashMap<>();
+        while (newPropFileMatcher.find()) {
+            String[] s = newPropFileMatcher.group().split(":");
+            if (s[DEFAULT_VALUE_PLACE].equals(SECRET_REPLACEMENT_FORMAT)) {
+                emptySecrets.put(s[DEFAULT_NAME_PLACE], newPropFileMatcher.group());
+            }
+        }
+        return emptySecrets;
+    }
+}
