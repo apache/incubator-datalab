a49b9be82c667aec05ee612b1b4a3dfb427444d7
leonidfrolov <frolovleon@gmail.com>
2021-02-08 15:40:09 +0200

    [DATALAB-2091]: fixed typo

--- infrastructure-provisioning/src/general/scripts/azure/ssn_configure.py
+++ infrastructure-provisioning/src/general/scripts/azure/ssn_configure.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
     try:
         logging.info('[INSTALLING PREREQUISITES TO SSN INSTANCE]')
         print('[INSTALLING PREREQUISITES TO SSN INSTANCE]')
-        params = "--hostname {} --keyfile {} --pip_packages 'backoff bcrypt==3.1.7 argparse babric3 pymongo pyyaml " \
+        params = "--hostname {} --keyfile {} --pip_packages 'backoff bcrypt==3.1.7 argparse fabric3 pymongo pyyaml " \
                  "pycrypto azure==2.0.0' --user {} --region {}".format(ssn_conf['instance_host'],
                                                                        ssn_conf['ssh_key_path'],
                                                                        ssn_conf['datalab_ssh_user'],
