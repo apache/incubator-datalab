f1aa0b22f0fdda425d310570aa9b5a49bce06ac4
leonidfrolov <frolovleon@gmail.com>
2021-02-12 16:04:34 +0200

    [DATALAB-2091]: added for testing

--- infrastructure-provisioning/src/base/scripts/create_ssh_user.py
+++ infrastructure-provisioning/src/base/scripts/create_ssh_user.py
@@ -39,7 +39,9 @@ args = parser.parse_args()
 
 
 def ensure_ssh_user(initial_user, os_user, sudo_group, conn):
+    print('ensure_ssh_user function')
     if not exists('/home/{}/.ssh_user_ensured'.format(initial_user)):
+        print('ensure_ssh_user function2')
         conn.sudo('useradd -m -G {1} -s /bin/bash {0}'.format(os_user, sudo_group))
         conn.sudo('echo "{} ALL = NOPASSWD:ALL" >> /etc/sudoers'.format(os_user))
         conn.sudo('mkdir /home/{}/.ssh'.format(os_user))
