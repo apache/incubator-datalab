02370a5f857a64cc36adabe4cb2552b7018bc3ca
leonidfrolov <frolovleon@gmail.com>
2021-02-22 16:17:30 +0200

    [DATALAB-2091]: removed \n from variable assigned from remote command output

--- infrastructure-provisioning/src/ssn/scripts/configure_ssn_node.py
+++ infrastructure-provisioning/src/ssn/scripts/configure_ssn_node.py
@@ -142,7 +142,7 @@ def configure_ssl_certs(hostname, custom_ssl_cert):
                 conn.sudo('mkdir -p /home/{0}/keys'.format(args.os_user))
                 conn.sudo('''bash -c 'echo "{0}" | base64 --decode > /etc/ssl/certs/root_ca.crt' '''.format(
                      os.environ['conf_stepcerts_root_ca']))
-                fingerprint = conn.sudo('step certificate fingerprint /etc/ssl/certs/root_ca.crt').stdout
+                fingerprint = conn.sudo('step certificate fingerprint /etc/ssl/certs/root_ca.crt').stdout.replace('\n', '')
                 conn.sudo('step ca bootstrap --fingerprint {0} --ca-url "{1}"'.format(fingerprint,
                                                                                  os.environ['conf_stepcerts_ca_url']))
                 conn.sudo('echo "{0}" > /home/{1}/keys/provisioner_password'.format(
