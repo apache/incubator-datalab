be3b013203628135439a91a32320557933a04bf2
leonidfrolov <frolovleon@gmail.com>
2021-02-22 14:37:35 +0200

    [DATALAB-2091]: fixed syntax error

--- infrastructure-provisioning/src/ssn/scripts/configure_ssn_node.py
+++ infrastructure-provisioning/src/ssn/scripts/configure_ssn_node.py
@@ -214,7 +214,7 @@ if __name__ == "__main__":
     print("Configure connections")
     try:
         global conn
-    conn = datalab.fab.init_datalab_connection(args.hostname, args.os_user, args.keyfile)
+        conn = datalab.fab.init_datalab_connection(args.hostname, args.os_user, args.keyfile)
         host_string = args.os_user + '@' + args.hostname
         deeper_config = json.loads(args.additional_config)
     except:
