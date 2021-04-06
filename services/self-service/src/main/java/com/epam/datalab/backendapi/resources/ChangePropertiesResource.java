06824a75c84ef355c0c776d7db5e68280f57170a
KinashYurii <ura.kinash@gmail.com>
2021-04-06 10:05:36 +0300

    [DATALAB-2314] -- fixed request for admin page

--- services/self-service/src/main/java/com/epam/datalab/backendapi/resources/ChangePropertiesResource.java
+++ services/self-service/src/main/java/com/epam/datalab/backendapi/resources/ChangePropertiesResource.java
@@ -17,7 +17,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("config")
+@Path("/config")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ChangePropertiesResource implements ChangePropertiesConst {
