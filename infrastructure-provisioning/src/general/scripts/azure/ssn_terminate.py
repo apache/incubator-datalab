2215e1a8569f0e28fc02fed758102b0ffec5f196
leonidfrolov <frolovleon@gmail.com>
2021-04-27 13:03:01 +0300

    [DATALAB-2091]: added additional output during azure ssn termination

--- infrastructure-provisioning/src/general/scripts/azure/ssn_terminate.py
+++ infrastructure-provisioning/src/general/scripts/azure/ssn_terminate.py
@@ -47,6 +47,8 @@ def terminate_ssn_node(resource_group_name, service_base_name, vpc_name, region)
     print("Removing network interfaces")
     try:
         for network_interface in AzureMeta.list_network_interfaces(resource_group_name):
+            print("======1")
+            print(network_interface)
             if service_base_name == network_interface.tags["SBN"]:
                 AzureActions.delete_network_if(resource_group_name, network_interface.name)
                 print("Network interface {} has been removed".format(network_interface.name))
