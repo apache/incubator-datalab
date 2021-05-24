5f66017a460cfb10a6cf2d95ed33ee93b1304f88
leonidfrolov <frolovleon@gmail.com>
2021-05-24 16:07:55 +0300

    [DATALAB-2333]: added leftover policy detach from the role

--- infrastructure-provisioning/src/general/lib/aws/actions_lib.py
+++ infrastructure-provisioning/src/general/lib/aws/actions_lib.py
@@ -957,8 +957,7 @@ def remove_all_iam_resources(instance_type, project_name='', endpoint_name=''):
                     attached_role_policies = client.list_attached_role_policies(RoleName=iam_role)
                     if attached_role_policies:
                         for policy in attached_role_policies['AttachedPolicies']:
-                            print('========1')
-                            print(policy)
+                            client.detach_role_policy(RoleName=iam_role, PolicyArn=policy['PolicyArn'])
                     role_profiles = client.list_instance_profiles_for_role(RoleName=iam_role).get('InstanceProfiles')
                     if role_profiles:
                         for i in role_profiles:
