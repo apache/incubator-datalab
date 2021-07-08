a3c13ef3af83883d84c917f1a8a65d81edb77363
KinashYurii <ura.kinash@gmail.com>
2021-07-08 15:51:36 +0300

    [DATALAB-2502] -- removed pip2, added Java lib for GCP_DP,GCP_Tensor

--- services/self-service/src/main/java/com/epam/datalab/backendapi/service/impl/LibraryServiceImpl.java
+++ services/self-service/src/main/java/com/epam/datalab/backendapi/service/impl/LibraryServiceImpl.java
@@ -22,6 +22,7 @@ package com.epam.datalab.backendapi.service.impl;
 import com.epam.datalab.auth.UserInfo;
 import com.epam.datalab.backendapi.annotation.*;
 import com.epam.datalab.backendapi.dao.BaseDAO;
+import com.epam.datalab.backendapi.dao.ComputationalDAO;
 import com.epam.datalab.backendapi.dao.ExploratoryDAO;
 import com.epam.datalab.backendapi.dao.ExploratoryLibDAO;
 import com.epam.datalab.backendapi.domain.EndpointDTO;
@@ -70,24 +71,24 @@ public class LibraryServiceImpl implements LibraryService {
     private static final String COMPUTATIONAL_NOT_FOUND_MSG = "Computational with name %s was not found";
     private static final String LIB_ALREADY_INSTALLED = "Library %s is already installing";
 
-    @Inject
-    private ExploratoryDAO exploratoryDAO;
+    private final ExploratoryDAO exploratoryDAO;
+    private final ExploratoryLibDAO libraryDAO;
+    private final RequestBuilder requestBuilder;
+    private final RESTService provisioningService;
+    private final RequestId requestId;
+    private final EndpointService endpointService;
 
     @Inject
-    private ExploratoryLibDAO libraryDAO;
-
-    @Inject
-    private RequestBuilder requestBuilder;
-
-    @Named(ServiceConsts.PROVISIONING_SERVICE_NAME)
-    @Inject
-    private RESTService provisioningService;
-
-    @Inject
-    private RequestId requestId;
-
-    @Inject
-    private EndpointService endpointService;
+    public LibraryServiceImpl(ExploratoryDAO exploratoryDAO, ExploratoryLibDAO libraryDAO, RequestBuilder requestBuilder,
+                              @Named(ServiceConsts.PROVISIONING_SERVICE_NAME) RESTService provisioningService,
+                              RequestId requestId, EndpointService endpointService, ComputationalDAO computationalDAO) {
+        this.exploratoryDAO = exploratoryDAO;
+        this.libraryDAO = libraryDAO;
+        this.requestBuilder = requestBuilder;
+        this.provisioningService = provisioningService;
+        this.requestId = requestId;
+        this.endpointService = endpointService;
+    }
 
 
     @Override
@@ -100,7 +101,6 @@ public class LibraryServiceImpl implements LibraryService {
             Document document = (Document) libraryDAO.findComputationalLibraries(user, project,
                     exploratoryName, computationalName)
                     .getOrDefault(ExploratoryLibDAO.COMPUTATIONAL_LIBS, new Document());
-
             return (List<Document>) document.getOrDefault(computationalName, new ArrayList<>());
         }
     }
