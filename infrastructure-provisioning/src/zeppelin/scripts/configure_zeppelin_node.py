8b3e86b6c1a30befab999ebf56d5bd65969bea55
oleh_mykolaishyn <owlleg6@gmail.com>
2021-11-04 10:46:45 +0200

    [DATALAB-2542]: Custom Zeppelin

--- infrastructure-provisioning/src/zeppelin/scripts/configure_zeppelin_node.py
+++ infrastructure-provisioning/src/zeppelin/scripts/configure_zeppelin_node.py
@@ -57,10 +57,9 @@ spark_version = args.spark_version
 hadoop_version = args.hadoop_version
 scala_link = "https://www.scala-lang.org/files/archive/"
 zeppelin_version = args.zeppelin_version
+zeppelin_link = "https://nexus.develop.dlabanalytics.com/repository/packages-public/zeppelin-"+ zeppelin_version +"-prebuilt.tar.gz"
 python_venv_version = os.environ['notebook_python_venv_version']
 python_venv_path = '/opt/python/python{0}/bin/python{1}'.format(python_venv_version, python_venv_version[:3])
-zeppelin_link = "https://archive.apache.org/dist/zeppelin/zeppelin-" + zeppelin_version + "/zeppelin-" + \
-                zeppelin_version + "-bin-netinst.tgz"
 if args.region == 'cn-north-1':
     spark_link = "http://mirrors.hust.edu.cn/apache/spark/spark-" + spark_version + "/spark-" + spark_version + \
                  "-bin-hadoop" + hadoop_version + ".tgz"
@@ -81,11 +80,12 @@ gitlab_certfile = os.environ['conf_gitlab_certfile']
 def configure_zeppelin(os_user):
     if not exists(conn,'/home/' + os_user + '/.ensure_dir/zeppelin_ensured'):
         try:
-            conn.sudo('wget ' + zeppelin_link + ' -O /tmp/zeppelin-' + zeppelin_version + '-bin-netinst.tgz')
-            conn.sudo('tar -zxvf /tmp/zeppelin-' + zeppelin_version + '-bin-netinst.tgz -C /opt/')
-            conn.sudo('ln -s /opt/zeppelin-' + zeppelin_version + '-bin-netinst /opt/zeppelin')
+            # test nexus
+            conn.sudo('wget ' + zeppelin_link + ' -O /tmp/zeppelin-' + zeppelin_version + '-prebuilt.tar.gz')
+            conn.sudo('tar -zxvf /tmp/zeppelin-' + zeppelin_version + '-prebuilt.tar.gz -C /opt/')
+            conn.sudo('ln -s /opt/zeppelin-' + zeppelin_version + '-prebuilt.tar.gz /opt/zeppelin')
             conn.sudo('cp /opt/zeppelin/conf/zeppelin-env.sh.template /opt/zeppelin/conf/zeppelin-env.sh')
-            java_home = conn.run("update-alternatives --query java | grep -o \'/.*/java-8.*/jre\'").stdout.splitlines()[0].replace('\n','')
+            java_home = conn.run("update-alternatives --query java | grep -o \'/.*/java-8.*/jre\'").stdout.splitlines()[0].replace('\n', '')
             conn.sudo('''bash -c "echo 'export JAVA_HOME=\'{}\'' >> /opt/zeppelin/conf/zeppelin-env.sh" '''.format(java_home))
             conn.sudo('cp /opt/zeppelin/conf/zeppelin-site.xml.template /opt/zeppelin/conf/zeppelin-site.xml')
             conn.sudo('sed -i \"/# export ZEPPELIN_PID_DIR/c\export ZEPPELIN_PID_DIR=/var/run/zeppelin\" /opt/zeppelin/conf/zeppelin-env.sh')
@@ -95,15 +95,16 @@ def configure_zeppelin(os_user):
             conn.sudo('sed -i \'s/127.0.0.1/0.0.0.0/g\' /opt/zeppelin/conf/zeppelin-site.xml')
             conn.sudo('mkdir /var/log/zeppelin')
             conn.sudo('mkdir /var/run/zeppelin')
-            conn.sudo('ln -s /var/log/zeppelin /opt/zeppelin-' + zeppelin_version + '-bin-netinst/logs')
+            conn.sudo('ln -s /var/log/zeppelin /opt/zeppelin/logs')
             conn.sudo('chown ' + os_user + ':' + os_user + ' -R /var/log/zeppelin')
-            conn.sudo('ln -s /var/run/zeppelin /opt/zeppelin-' + zeppelin_version + '-bin-netinst/run')
+            conn.sudo('ln -s /var/run/zeppelin /opt/zeppelin/run')
             conn.sudo('chown ' + os_user + ':' + os_user + ' -R /var/run/zeppelin')
             conn.sudo('''bash -l -c '/opt/zeppelin/bin/install-interpreter.sh --name {} --proxy-url $http_proxy' '''.format(zeppelin_interpreters))
-            conn.sudo('chown ' + os_user + ':' + os_user + ' -R /opt/zeppelin-' + zeppelin_version + '-bin-netinst')
+            conn.sudo('''bash -l -c '/opt/zeppelin/bin/install-interpreter.sh --name sh --artifact /opt/zeppelin/interpreter/sh/zeppelin-shell-*.jar --proxy-url $http_proxy' ''')
+            conn.sudo('chown ' + os_user + ':' + os_user + ' -R /opt/zeppelin')
             conn.sudo('mkdir -p /opt/zeppelin/lib/interpreter/')
-            conn.sudo('cp /opt/zeppelin-' + zeppelin_version + '-bin-netinst/interpreter/md/zeppelin-markdown-*.jar /opt/zeppelin/lib/interpreter/') # necessary when executing paragraph launches java process with "-cp :/opt/zeppelin/lib/interpreter/*:"
-            conn.sudo('cp /opt/zeppelin-' + zeppelin_version + '-bin-netinst/interpreter/sh/zeppelin-shell-*.jar /opt/zeppelin/lib/interpreter/')
+            conn.sudo('cp /opt/zeppelin/interpreter/md/zeppelin-markdown-*.jar /opt/zeppelin/lib/interpreter/')  # necessary when executing paragraph launches java process with "-cp :/opt/zeppelin/lib/interpreter/*:"
+            conn.sudo('cp /opt/zeppelin/interpreter/sh/zeppelin-shell-*.jar /opt/zeppelin/lib/interpreter/')
         except Exception as err:
             print('Error:', str(err))
             sys.exit(1)
@@ -169,12 +170,14 @@ def configure_local_spark_kernels(args, python_venv_path):
         conn.sudo('sed -i "s|OS_USER|' + args.os_user + '|g" /tmp/interpreter.json')
         spark_memory = get_spark_memory()
         conn.sudo('sed -i "s|DRIVER_MEMORY|{}m|g" /tmp/interpreter.json'.format(spark_memory))
-        conn.sudo('sed -i "s|PYTHON_VENV_PATH|{}m|g" /tmp/interpreter.json'.format(python_venv_path))
+        conn.sudo('sed -i "s|PYTHON_VENV_PATH|{}|g" /tmp/interpreter.json'.format(python_venv_path))
         update_zeppelin_interpreters(args.multiple_clusters, r_enabled, 'local')
         conn.sudo('cp -f /tmp/interpreter.json /opt/zeppelin/conf/interpreter.json')
         conn.sudo('chown ' + args.os_user + ':' + args.os_user + ' -R /opt/zeppelin/')
         conn.sudo('touch /home/' + args.os_user + '/.ensure_dir/local_spark_kernel_ensured')
+    conn.sudo("systemctl stop zeppelin-notebook")
     conn.sudo("systemctl daemon-reload")
+    conn.sudo("systemctl enable zeppelin-notebook")
     conn.sudo("systemctl start zeppelin-notebook")
 
 
