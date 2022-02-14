bf6d18f0358aa7a490ab87becb28f325a50cedeb
leonidfrolov <frolovleon@gmail.com>
2022-02-14 18:41:50 +0200

    [DATALAB-2698]: added venv libs install

--- infrastructure-provisioning/src/tensor/scripts/configure_tensor_node.py
+++ infrastructure-provisioning/src/tensor/scripts/configure_tensor_node.py
@@ -69,7 +69,7 @@ cuda_version = os.environ['notebook_cuda_version']
 cuda_file_name = os.environ['notebook_cuda_file_name']
 cudnn_version = os.environ['notebook_cudnn_version']
 cudnn_file_name = os.environ['notebook_cudnn_file_name']
-
+venv_libs = 'numpy scipy matplotlib pandas scikit-learn opencv-python'
 
 ##############
 # Run script #
@@ -147,7 +147,10 @@ if __name__ == "__main__":
         ensure_pytorch(args.os_user)
     print("Install Matplotlib")
     ensure_matplot(args.os_user)
-    
+
+    print("Install python venv required libs")
+    ensure_venv_libs(args.os_user, venv_libs)
+
     #POST INSTALLATION PROCESS
     print("Updating pyOpenSSL library")
     update_pyopenssl_lib(args.os_user)
