3b255efc22f262724c21c4a65327a1b4d8deac28
Denys Yankiv <denys.yankiv@gmail.com>
2022-03-16 13:26:49 +0200

    fix java lib

--- services/self-service/src/main/java/com/epam/datalab/backendapi/domain/NotebookTemplate.java
+++ services/self-service/src/main/java/com/epam/datalab/backendapi/domain/NotebookTemplate.java
@@ -26,8 +26,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum NotebookTemplate {
     JUPYTER("Jupyter notebook 6.1.6"),
-    JUPYTER_GPU("Jupyter with GPU notebook 6.1.6"),
-    JUPYTER_GPU_CONDA("Jupyter with GPU and Anaconda notebook 6.1.6"),
+    JUPYTER_GPU("Jupyter notebook 6.1.6 with GPU"),
+    JUPYTER_GPU_CONDA("Jupyter notebook 6.1.6 with GPU and Anaconda"),
     JUPYTER_LAB("JupyterLab 0.35.6"),
     ZEPPELIN("Apache Zeppelin 0.9.1"),
     DEEP_LEARNING("Deep Learning  2.4"),
