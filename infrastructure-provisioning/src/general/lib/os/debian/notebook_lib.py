58792b4aa0f14e3632dde0352b882f38c2901e20
oleh_mykolaishyn <owlleg6@gmail.com>
2022-03-17 12:44:01 +0200

    fixed indent

--- infrastructure-provisioning/src/general/lib/os/debian/notebook_lib.py
+++ infrastructure-provisioning/src/general/lib/os/debian/notebook_lib.py
@@ -412,7 +412,7 @@ def ensure_pytorch(os_user, gpu=True):
                                                  'torchaudio==0.10.0+cu111 -f '
                                                  'https://download.pytorch.org/whl/cu111/torch_stable.html')
             else:
-            datalab.fab.install_venv_pip_pkg('torch==1.10.2+cu113 torchvision==0.11.3+cu113 '
+                datalab.fab.install_venv_pip_pkg('torch==1.10.2+cu113 torchvision==0.11.3+cu113 '
                                                   'torchaudio==0.10.2+cu113 -f '
                                                   'https://download.pytorch.org/whl/cu113/torch_stable.html')
         else:
