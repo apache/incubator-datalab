e555e63b4f34b5c9cc0333352017462f96f1a71c
Pavel Papou <ppapou27@gmail.com>
2022-04-01 15:00:22 -0400

    [DATALAB-2245] Audit report - code grooming
     - modules have been groomed

--- services/self-service/src/main/java/com/epam/datalab/backendapi/util/AuditUtils.java
+++ services/self-service/src/main/java/com/epam/datalab/backendapi/util/AuditUtils.java
@@ -1,8 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.epam.datalab.backendapi.util;
 
 import com.epam.datalab.backendapi.domain.AuditReportLine;
-import com.epam.datalab.backendapi.domain.BillingReportLine;
-import com.epam.datalab.dto.UserInstanceStatus;
 import jersey.repackaged.com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
 
@@ -11,6 +28,9 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.*;
 
+/**
+ * Class helper to handle the audit records
+ */
 public class AuditUtils {
 
     private static final String REPORT_FIRST_LINE = "Available reporting period from: %s to: %s";
@@ -30,6 +50,16 @@ public class AuditUtils {
                 CSVFormatter.SEPARATOR, '\"');
     }
 
+    /**
+     *
+     * @param line - Audit Report line objet, which contains exported rows
+     * @return formatted string of the Audit reportL
+     * Date,User,Action,Project,Resource type,Resource
+     * 2022-03-20,test,LOG_IN,,,
+     * 2022-03-20,test,LOG_IN,,,
+     * 2022-03-19,test,LOG_IN,,,
+     * 2022-03-17,test,LOG_IN,,,
+     */
     public static String printLine(AuditReportLine line) {
         List<String> lines = new ArrayList<>();
         lines.add(getOrEmpty(line.getTimestamp().toString()));
@@ -46,8 +76,6 @@ public class AuditUtils {
     }
 
     private static String getOrEmpty(String s) {
-
         return Objects.nonNull(s) ? s : StringUtils.EMPTY;
     }
-
 }
