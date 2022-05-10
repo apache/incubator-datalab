de6b274f5a66acc0443814d5605a97acf6389691
Oleksandr Polishchuk2 <oleksandr_polishchuk2@epam.com>
2022-05-10 17:29:55 +0300

    [DATALAB-2768]: fixed issue with 'other' group libs

--- infrastructure-provisioning/src/general/scripts/os/get_list_available_pkgs.py
+++ infrastructure-provisioning/src/general/scripts/os/get_list_available_pkgs.py
@@ -69,8 +69,8 @@ def get_available_pip_pkgs(version):
 def get_uncategorised_pip_pkgs(all_pkgs_pip2, all_pkgs_pip3):
     try:
         pip_pkgs = dict()
-        attempt = 0
-        while attempt < 3:
+        attempts = 0
+        while attempts < 3:
             try:
                 client = xmlrpc.client.ServerProxy('https://pypi.python.org/pypi')
                 raw_pkgs = client.list_packages()
@@ -78,11 +78,11 @@ def get_uncategorised_pip_pkgs(all_pkgs_pip2, all_pkgs_pip3):
                 for pkg in raw_pkgs:
                     if pkg not in all_pkgs_pip2 and pkg not in all_pkgs_pip3:
                         all_pkgs_other.append(pkg)
-                        for pkg in all_pkgs_other:
-                            pip_pkgs[pkg] = "N/A"
-                        return pip_pkgs
+                for pkg in all_pkgs_other:
+                    pip_pkgs[pkg] = "N/A"
+                return pip_pkgs
             except:
-                attempt += 1
+                attempts += 1
                 time.sleep(10)
         if attempt == 3:
             logging.info("Unable to get uncategorised pip packages")
