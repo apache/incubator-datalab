3916099d7d9118afbf611cc0d420d225a880dc0c
Denys Yankiv <denys.yankiv@gmail.com>
2022-05-18 14:18:24 +0300

    add support to create user image as admin

--- services/self-service/src/main/java/com/epam/datalab/backendapi/service/impl/EnvironmentServiceImpl.java
+++ services/self-service/src/main/java/com/epam/datalab/backendapi/service/impl/EnvironmentServiceImpl.java
@@ -66,19 +66,21 @@ public class EnvironmentServiceImpl implements EnvironmentService {
     private final ComputationalService computationalService;
     private final SecurityService securityService;
     private final ProjectService projectService;
+    private final ImageExploratoryService imageExploratoryService;
 
     @Inject
     public EnvironmentServiceImpl(EnvDAO envDAO, UserSettingsDAO settingsDAO, ExploratoryDAO exploratoryDAO,
                                   ExploratoryService exploratoryService, ComputationalService computationalService,
-                                  SecurityService securityService, ProjectService projectService) {
-        this.envDAO = envDAO;
-        this.settingsDAO = settingsDAO;
-        this.exploratoryDAO = exploratoryDAO;
-        this.exploratoryService = exploratoryService;
-        this.computationalService = computationalService;
-        this.securityService = securityService;
-        this.projectService = projectService;
-    }
+        SecurityService securityService, ProjectService projectService, ImageExploratoryService imageExploratoryService) {
+            this.envDAO = envDAO;
+            this.settingsDAO = settingsDAO;
+            this.exploratoryDAO = exploratoryDAO;
+            this.exploratoryService = exploratoryService;
+            this.computationalService = computationalService;
+            this.securityService = securityService;
+            this.projectService = projectService;
+            this.imageExploratoryService = imageExploratoryService;
+        }
 
     @Override
     public List<UserDTO> getUsers() {
@@ -105,6 +107,12 @@ public class EnvironmentServiceImpl implements EnvironmentService {
                 .map(projectDTO -> getProjectEnv(projectDTO, expList))
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList());
+    }
+
+    @ProjectAdmin
+    @Override
+    public void createImage(@User UserInfo userInfo, String user, @Project String project, String exploratoryName, String imageName, String description) {
+        imageExploratoryService.createImage(securityService.getUserInfoOffline(user), project, exploratoryName, imageName, description, null);
     }
 
     @ProjectAdmin
