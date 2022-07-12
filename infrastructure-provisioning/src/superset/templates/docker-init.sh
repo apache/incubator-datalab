feb2575a7bdb8eeb7e31a991e43d273c4729bbb0
leonidfrolov <frolovleon@gmail.com>
2022-07-12 12:47:36 +0300

    [DATALAB-2890] superset version update changes

--- infrastructure-provisioning/src/superset/templates/docker-init.sh
+++ infrastructure-provisioning/src/superset/templates/docker-init.sh
@@ -15,19 +15,59 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-set -ex
+set -e
 
-# Create an admin user (you will be prompted to set username, first and last name before setting a password)
-export FLASK_APP=superset:app
-#flask fab create-admin --username admin --firstname admin --lastname admin --password admin
+#
+# Always install local overrides first
+#
+/app/docker/docker-bootstrap.sh
 
+STEP_CNT=4
+
+echo_step() {
+cat <<EOF
+######################################################################
+Init Step ${1}/${STEP_CNT} [${2}] -- ${3}
+######################################################################
+EOF
+}
+ADMIN_PASSWORD="admin"
+# If Cypress run – overwrite the password for admin and export env variables
+if [ "$CYPRESS_CONFIG" == "true" ]; then
+    ADMIN_PASSWORD="general"
+    export SUPERSET_CONFIG=tests.integration_tests.superset_test_config
+    export SUPERSET_TESTENV=true
+    export ENABLE_REACT_CRUD_VIEWS=true
+    export SUPERSET__SQLALCHEMY_DATABASE_URI=postgresql+psycopg2://superset:superset@db:5432/superset
+fi
 # Initialize the database
+echo_step "1" "Starting" "Applying DB migrations"
 superset db upgrade
+echo_step "1" "Complete" "Applying DB migrations"
+
+# Create an admin user
+echo_step "2" "Starting" "Setting up admin user ( admin / $ADMIN_PASSWORD )"
+superset fab create-admin \
+              --username admin \
+              --firstname Superset \
+              --lastname Admin \
+              --email admin@superset.com \
+              --password $ADMIN_PASSWORD
+echo_step "2" "Complete" "Setting up admin user"
+# Create default roles and permissions
+echo_step "3" "Starting" "Setting up roles and perms"
+superset init
+echo_step "3" "Complete" "Setting up roles and perms"
 
 if [ "$SUPERSET_LOAD_EXAMPLES" = "yes" ]; then
     # Load some data to play with
-    superset load_examples
+    echo_step "4" "Starting" "Loading examples"
+    # If Cypress run which consumes superset_test_config – load required data for tests
+    if [ "$CYPRESS_CONFIG" == "true" ]; then
+        superset load_test_users
+        superset load_examples --load-test-data
+    else
+        superset load_examples
+    fi
+    echo_step "4" "Complete" "Loading examples"
 fi
-
-# Create default roles and permissions
-superset init
