02b980720d06ea8880f15f0b0954d1a98758c8c8
Oleksandr Polishchuk <polishchuk500@gmail.com>
2022-07-28 16:20:31 +0300

    [DATALAB-2309]: changed notebook links to lowercase on AWS

--- infrastructure-provisioning/src/general/scripts/aws/common_prepare_notebook.py
+++ infrastructure-provisioning/src/general/scripts/aws/common_prepare_notebook.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
             sys.exit(1)
         logging.info('Generating infrastructure names and tags')
         try:
-            notebook_config['exploratory_name'] = (os.environ['exploratory_name']).replace('_', '-').lower()
+            notebook_config['exploratory_name'] = os.environ['exploratory_name'].lower()
         except:
             notebook_config['exploratory_name'] = ''
 
