3aa080ff333e67965fa1aba5762ae29d0114dc6b
Denys Yankiv <denys.yankiv@gmail.com>
2022-10-03 13:36:00 +0300

    fix delete platform

--- services/self-service/src/main/java/com/epam/datalab/backendapi/dao/ConnectedPlatformsDAO.java
+++ services/self-service/src/main/java/com/epam/datalab/backendapi/dao/ConnectedPlatformsDAO.java
@@ -32,5 +32,5 @@ public interface ConnectedPlatformsDAO {
 
     List<ConnectedPlatformDTO> getUserPlatforms(String userName);
 
-    void delete(String name);
+    void delete(String user, String name);
 }
