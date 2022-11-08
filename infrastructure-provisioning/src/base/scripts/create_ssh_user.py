d49fa94ad33a9ad18f133abaff38f1d8146b91de
leonidfrolov <frolovleon@gmail.com>
2022-11-08 10:54:34 +0200

    [DATALAB-3099]: added pause before creating user

--- infrastructure-provisioning/src/base/scripts/create_ssh_user.py
+++ infrastructure-provisioning/src/base/scripts/create_ssh_user.py
@@ -43,6 +43,7 @@ args = parser.parse_args()
 
 def ensure_ssh_user(initial_user, os_user, sudo_group):
     if not exists(conn, '/home/{}/.ssh_user_ensured'.format(initial_user)):
+        time.sleep(180)
         conn.sudo('useradd -m -G {1} -s /bin/bash {0}'.format(os_user, sudo_group))
         conn.sudo('bash -c "echo \'{} ALL = NOPASSWD:ALL\' >> /etc/sudoers"'.format(os_user))
         conn.sudo('mkdir /home/{}/.ssh'.format(os_user))
